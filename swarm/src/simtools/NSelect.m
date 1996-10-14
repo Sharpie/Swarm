72c4788dc20694c4238e1e49b672631f1d375b68
manor <manor@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1996-10-14 19:16:14 +0000

    (manor): Forgot the .m file... used the .h by mistake.
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@17 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- swarm/src/simtools/NSelect.m
+++ swarm/src/simtools/NSelect.m
@@ -3,14 +3,56 @@
 // implied warranty of merchantability or fitness for a particular purpose.
 // See file LICENSE for details and terms of copying.
 
-// NSelect -> select exactly N elements from a collection 
-// without repetition! The target collection must be provided.
+/*
+Name:         NSelect.m
+Description:  Selection Routine
+Library:      simtools
+*/
 
-#import <swarmobject.h>
+#define __USE_FIXED_PROTOTYPES__  // for gcc headers
+#import <stdlib.h>
+#import <simtools.h>
 
-@interface NSelect : SwarmObject {
+@implementation NSelect
+
++(void) select: (int) n from: aCollection into: bCollection {
+  id a, b ;
+  int N ; // total number of items in aCollection
+  int t ; // items seen
+  int m ; // items selected
+  float r ;
+
+  if(!n)
+    return ;
+
+  t = m = 0 ;
+
+  N = [aCollection getCount] ;
+
+  if(N < n){
+    fprintf("NSelect: attempted to select %d elements from a collection containing only %d elements!!!\n",n,N) ;
+    exit(-1) ;
+  }
+
+  a = [aCollection begin: scratchZone] ;
+  b = [bCollection begin: scratchZone] ;
+
+  while(m < n){
+    r = [uniformRandom rFloat] ;    
+    
+    if( ( ((float)(N - t)) * r) >= ((float)(n - m)) ){
+      [a next] ;
+    } else {
+      m++ ;
+      [b next] ;
+      [b put: [a next]] ;
+    }
+
+    t++ ;
+  }
+
+  [a drop] ;
+  [b drop] ;
 }
-
-+(void) select: (int) n from: aCollection into: bCollection ;
 
 @end
