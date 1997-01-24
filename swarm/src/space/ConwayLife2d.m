1e3991d442405e961e6f28f6a7d266ce6bdd24df
gepr <gepr@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1997-01-24 21:37:33 +0000

    Made some changes for compatibility with new random lib.
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@42 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- swarm/src/space/ConwayLife2d.m
+++ swarm/src/space/ConwayLife2d.m
@@ -20,7 +20,7 @@
 
   for (y = 0; y < ysize; y++)
     for (x = 0; x < xsize; x++) {
-      int newState = [uniformRandom rMax: 3] == 2;
+      int newState = [uniformRandom getIntegerWithMin: 0L withMax: 2L] == 2;
       [self putValue: newState atX: x Y: y];
     }
   [self updateLattice];
