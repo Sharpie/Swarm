2e451c21a2ebc6b9eb01e3afa3706007ee897b64
swarm <swarm@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1997-11-16 18:05:17 +0000

    Move complicated and redundant drag&drop calls to globalTkInterp in simtools/ into functions in tkobjc.  These functions also serve to dynamically adjust to the version of BLT that is being used; there was a interface change between BLT2.1 and BLT2.3 in the drag&drop interface.
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@105 84d386b0-d280-4341-8980-c4cb7e1eb7a5

---
+++ swarm/src/tkobjc/DragDrop.h
@@ -0,0 +1,9 @@
+// Swarm library. Copyright (C) 1996-1997 Santa Fe Institute.
+// This library is distributed without any warranty; without even the
+// implied warranty of merchantability or fitness for a particular purpose.
+// See file LICENSE for details and terms of copying.
+
+void dragAndDrop (id source, id object);
+void dragAndDropArg (id source, id object, int arg);
+void dragAndDropTarget (id target, id object);
+
---
+++ swarm/src/tkobjc/DragDrop.m
@@ -0,0 +1,65 @@
+// Swarm library. Copyright (C) 1996-1997 Santa Fe Institute.
+// This library is distributed without any warranty; without even the
+// implied warranty of merchantability or fitness for a particular purpose.
+// See file LICENSE for details and terms of copying.   
+
+#import <tkobjc/global.h>
+#import <tkobjc/Widget.h>
+
+static const char *BltDragAndDropConfigTag (void)
+{
+  const char *version_string = [globalTkInterp getBltVersion];
+  int major, minor;
+  
+  sscanf (version_string, "%d.%d", &major, &minor);
+  return (major >= 2 && minor >= 3) ? "" : "config";
+}
+
+void dragAndDropTarget (id target, id object)
+{
+  [globalTkInterp
+    eval:
+      "drag&drop target %s handler id {%s idReceive}",
+    [target getWidgetName],
+    tclObjc_objectToName (object)];
+}
+
+void dragAndDrop (id source, id object)
+{
+  const char *sourceWidgetName = [source getWidgetName],
+    *objectName = tclObjc_objectToName (object);
+
+  [globalTkInterp
+    eval: "drag&drop source %s %s -packagecmd {do_package %s} -sitecmd sitecmd -button 1", 
+    sourceWidgetName,
+    BltDragAndDropConfigTag (),
+    objectName];
+
+  [globalTkInterp
+    eval: "drag&drop source %s handler id send_id", 
+    sourceWidgetName,
+    objectName];
+}
+
+void dragAndDropArg (id source, id object, int arg)
+{
+  const char *sourceWidgetName = [source getWidgetName],
+    *objectName = tclObjc_objectToName (object);
+  
+  [globalTkInterp
+    eval: "drag&drop target %s handler id {%s idReceive: %d}", 
+    sourceWidgetName,
+    objectName,
+    arg];
+
+  [globalTkInterp
+    eval: "drag&drop source %s %s -packagecmd {do_package_arg %s %d} -sitecmd sitecmd -button 1",
+    sourceWidgetName,
+    BltDragAndDropConfigTag (),
+    objectName,
+    arg];
+  
+  [globalTkInterp
+    eval: "drag&drop source %s handler id send_id", 
+    sourceWidgetName];
+}
