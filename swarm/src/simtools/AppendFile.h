913379d575f14d7873357220cd11bef1fce77828
alex <alex@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1997-12-02 04:41:29 +0000

    New class to allow text to be appended to the end of a file. Subclasses from OutFile and overwrites the +createBegin and opens the file in append mode.
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@158 84d386b0-d280-4341-8980-c4cb7e1eb7a5

---
+++ swarm/src/simtools/AppendFile.h
@@ -0,0 +1,17 @@
+// Swarm library. Copyright (C) 1996 Santa Fe Institute.
+// This library is distributed without any warranty; without even the
+// implied warranty of merchantability or fitness for a particular purpose.
+// See file LICENSE for details and terms of copying.
+
+#import <simtools/OutFile.h>
+
+// this object opens the designated file in 'append' mode
+// rather than 'create' mode, thus not wiping out
+// an existing file.
+
+@interface AppendFile : OutFile {
+}
+
++create: aZone withName: (char *) theName ;
+
+@end
---
+++ swarm/src/simtools/AppendFile.m
@@ -0,0 +1,28 @@
+// Swarm library. Copyright (C) 1996 Santa Fe Institute.
+// This library is distributed without any warranty; without even the
+// implied warranty of merchantability or fitness for a particular purpose.
+// See file LICENSE for details and terms of copying.
+
+
+#define __USE_FIXED_PROTOTYPES__  // for gcc headers
+#import <stdio.h>
+#import <simtools/AppendFile.h>
+
+@implementation AppendFile
+
++create: aZone withName: (char *) theName {
+  FILE *aFile ;
+  id anObj ;
+
+  aFile = fopen(theName,"a") ;	// opens in "a" - append mode				
+  if(aFile == NULL){
+    fprintf(stderr,
+      "Unable to open %s as an AppendFile object!\n",theName) ;	
+    return nil ;
+  }
+
+  anObj = [AppendFile create: aZone] ;
+  [anObj _setFile_: aFile] ;
+  
+  return anObj ;
+}
