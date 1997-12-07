6ed60b9de1cf9172281a43401ea07d65d34ffe80
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1997-12-07 21:10:57 +0000

    Constify, minor reformatting.
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@179 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- swarm/src/tkobjc/Widget.h
+++ swarm/src/tkobjc/Widget.h
@@ -10,39 +10,39 @@
 
 @interface Widget : CreateDrop
 {
-  Widget * parent;
-  char * widgetName;
-  char * objcName;
+  Widget *parent;
+  const char *widgetName;
+  const char *objcName;
 }
 
 // creation time messages
--setParent: (Widget *) p;			  // set parent widget
--createEnd;					  // finalize creation
-+createParent: (Widget *)p;			  // convenience interface
+- setParent: (Widget *) p;			  // set parent widget
+- createEnd;					  // finalize creation
++ createParent: (Widget *)p;			  // convenience interface
 
--(char *) getWidgetName;			  // return the widget name
--(char *) getObjcName;				  // return the tclobjc name
--(Widget *) getParent;				  // return the parent
--(Widget *) getTopLevel;			  // return the top parent
+- (const char *)getWidgetName;			  // return the widget name
+- (const char *)getObjcName;			  // return the tclobjc name
+- (Widget *) getParent;				  // return the parent
+- (Widget *) getTopLevel;			  // return the top parent
 
--(const char *) getWindowGeometry;		  // get geometry as a string
--(unsigned) getWidth;				  // get geometry values
--(unsigned) getHeight;
--(int) getPositionX;
--(int) getPositionY;
+- (const char *)getWindowGeometry;		  // get geometry as a string
+- (unsigned)getWidth;				  // get geometry values
+- (unsigned)getHeight;
+- (int)getPositionX;
+- (int)getPositionY;
 
--setWidth: (unsigned) w Height: (unsigned) h;	  // set size
--setWidth: (unsigned) w;
--setHeight: (unsigned) h;
--setWindowGeometry: (const char *) s;             // set geometry as a string
--setPositionX: (int) x Y: (int) y;		  // set window position.
+- setWidth: (unsigned) w Height: (unsigned) h;	  // set size
+- setWidth: (unsigned) w;
+- setHeight: (unsigned) h;
+- setWindowGeometry: (const char *)s;             // set geometry as a string
+- setPositionX: (int) x Y: (int) y;		  // set window position.
 
--setWindowTitle: (char *) s;			  // window manager title
+- setWindowTitle: (const char *)s;		  // window manager title
 
--pack;						  // display the widget
--packWith: (char *) c;				  // display, args.
--unpack;					  // unmap the widget
+- pack;						  // display the widget
+- packWith: (const char *)c;			  // display, args.
+- unpack;					  // unmap the widget
 
--makeNameFromParentName: (char *) p;		  // make name for widget
+- makeNameFromParentName: (const char *)p;	  // make name for widget
 
 @end
