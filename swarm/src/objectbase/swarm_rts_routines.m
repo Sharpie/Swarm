6486b4e0ad2a839ec3d6c35f904f36be5846cfd9
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1997-12-10 21:23:12 +0000

    * swarm_rts_routines.m (isDigit): Add macro to cast isdigit argument char to int (to avoid array index using char compiler warning). Reformatting throughout. (my_objc_skip_typespec): Use new macro isDigit.
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@258 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- swarm/src/objectbase/swarm_rts_routines.m
+++ swarm/src/objectbase/swarm_rts_routines.m
@@ -8,8 +8,11 @@
 
 #import <objectbase/swarm_rts_routines.h>
 
+// Avoid using chars as an index to ctype table.
+#define isDigit(ch) isdigit((int)ch)
+
 inline const char*
-my_objc_skip_type_qualifiers (const char* type)
+my_objc_skip_type_qualifiers (const char *type)
 {
   while (*type == _C_CONST
 	 || *type == _C_IN 
@@ -17,89 +20,91 @@ my_objc_skip_type_qualifiers (const char* type)
 	 || *type == _C_OUT 
 	 || *type == _C_BYCOPY
 	 || *type == _C_ONEWAY)
-    {
-      type += 1;
-    }
+    type++;
   return type;
 }
+
 const char* 
-my_objc_skip_typespec (const char* type)
+my_objc_skip_typespec (const char * type)
 {
   type = my_objc_skip_type_qualifiers (type);
 
-  switch (*type) {
-
-  case _C_ID:
-    /* An id may be annotated by the actual type if it is known
-       with the @"ClassName" syntax */
-
-    if (*++type != '"')
-      return type;
-    else
-      {
-	while (*++type != '"') /* do nothing */;
-	return type + 1;
-      }
-
-    /* The following are one character type codes */
-  case _C_CLASS:
-  case _C_SEL:
-  case _C_CHR:
-  case _C_UCHR:
-  case _C_CHARPTR:
-  case _C_ATOM:
-  case _C_SHT:
-  case _C_USHT:
-  case _C_INT:
-  case _C_UINT:
-  case _C_LNG:
-  case _C_ULNG:
-  case _C_FLT:
-  case _C_DBL:
-  case _C_VOID:
-  case _C_UNDEF:
-    return ++type;
-    break;
-
-  case _C_ARY_B:
-    /* skip digits, typespec and closing ']' */
-    
-    while(isdigit(*++type));
-    type = my_objc_skip_typespec(type);
-    if (*type == _C_ARY_E)
+  switch (*type)
+    {
+    case _C_ID:
+      /* An id may be annotated by the actual type if it is known
+         with the @"ClassName" syntax */
+      
+      if (*++type != '"')
+        return type;
+      else
+        {
+          while (*++type != '"') /* do nothing */;
+          return type + 1;
+        }
+      
+      /* The following are one character type codes */
+    case _C_CLASS:
+    case _C_SEL:
+    case _C_CHR:
+    case _C_UCHR:
+    case _C_CHARPTR:
+    case _C_ATOM:
+    case _C_SHT:
+    case _C_USHT:
+    case _C_INT:
+    case _C_UINT:
+    case _C_LNG:
+    case _C_ULNG:
+    case _C_FLT:
+    case _C_DBL:
+    case _C_VOID:
+    case _C_UNDEF:
       return ++type;
-    else
+      break;
+      
+    case _C_ARY_B:
+      /* skip digits, typespec and closing ']' */
+      
+      while (isDigit (*++type));
+      type = my_objc_skip_typespec (type);
+      if (*type == _C_ARY_E)
+        return ++type;
+      else
+        abort();
+      
+    case _C_STRUCT_B:
+      /* skip name, and elements until closing '}'  */
+      
+      while (*type != _C_STRUCT_E && *type++ != '=');
+      while (*type != _C_STRUCT_E) 
+        type = my_objc_skip_typespec (type);
+      return ++type;
+      
+    case _C_UNION_B:
+      /* skip name, and elements until closing ')'  */
+      
+      while (*type != _C_UNION_E && *type++ != '=');
+      while (*type != _C_UNION_E)
+        type = my_objc_skip_typespec (type);
+      return ++type;
+      
+    case _C_PTR:
+      /* Just skip the following typespec */
+      
+      return my_objc_skip_typespec (++type);
+      
+    default:
       abort();
-
-  case _C_STRUCT_B:
-    /* skip name, and elements until closing '}'  */
-    
-    while (*type != _C_STRUCT_E && *type++ != '=');
-    while (*type != _C_STRUCT_E) { type = my_objc_skip_typespec (type); }
-    return ++type;
-
-  case _C_UNION_B:
-    /* skip name, and elements until closing ')'  */
-    
-    while (*type != _C_UNION_E && *type++ != '=');
-    while (*type != _C_UNION_E) { type = my_objc_skip_typespec (type); }
-    return ++type;
-
-  case _C_PTR:
-    /* Just skip the following typespec */
-    
-    return my_objc_skip_typespec (++type);
-    
-  default:
-    abort();
-  }
+    }
 }
 
 inline const char* 
 my_objc_skip_offset (const char* type)
 {
-  if (*type == '+') type++;
-  while(isdigit(*++type));
+  if (*type == '+')
+    type++;
+  while (_isdigit (*++type));
   return type;
 }
 
@@ -116,10 +121,11 @@ my_method_get_number_of_arguments (struct objc_method* mth)
 {
   int i = 0;
   const char* type = mth->method_types;
+
   while (*type)
     {
       type = my_objc_skip_argspec (type);
-      i += 1;
+      i++;
     }
   return i - 1;
 }
@@ -132,7 +138,7 @@ get_number_of_arguments (const char* type)
   while (*type)
     {
       type = my_objc_skip_argspec (type);
-      i += 1;
+      i++;
     }
   return i - 1;
 }
