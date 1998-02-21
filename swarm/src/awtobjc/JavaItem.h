8ff63fe03554941b70a47c0a8638115392efd391
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1998-02-21 06:16:43 +0000

    Add JavaItem.
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@603 84d386b0-d280-4341-8980-c4cb7e1eb7a5

---
+++ swarm/src/awtobjc/JavaItem.h
@@ -0,0 +1,10 @@
+#import <awtobjc/JavaObject.h>
+
+#define	ITEM_OVAL	0
+#define	ITEM_RECT	1
+
+@interface JavaItem: JavaObject
+{
+}
+
+@end
---
+++ swarm/src/awtobjc/JavaItem.m
@@ -0,0 +1,5 @@
+#import <awtobjc/JavaItem.h>
+
+@implementation JavaItem;
+
+@end
