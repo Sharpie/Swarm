31b7b970ccaaaba39674e6baf8d28887b45dca9c
alex <alex@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1998-02-26 01:37:31 +0000

    * Added #define DEFAULT_RANDOM_SEED (= 911).
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@630 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- apps/objc/graph/graph.h
+++ apps/objc/graph/graph.h
@@ -2,6 +2,8 @@
 #import <graph/DiGraphNode.h>
 #import <graph/DiGraphLink.h>
 
+#define DEFAULT_RANDOM_SEED    911
+
 extern id <Symbol> RectangleNode, OvalNode ; 
 extern void initGraphLibrary();
 
