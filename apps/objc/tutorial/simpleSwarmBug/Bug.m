5f8a504d9a202adfe69fba6d1945283a6cd3b2cc
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1998-02-28 08:38:22 +0000

    * Bug.m, FoodSpace.m: Include random.h instead of simtools.h. Reformatting throughout.
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@680 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- apps/objc/tutorial/simpleSwarmBug/Bug.m
+++ apps/objc/tutorial/simpleSwarmBug/Bug.m
@@ -1,46 +1,51 @@
 // Bug.m					SimpleBug
 
 #import "Bug.h"
-#import <simtools.h>
+#import <random.h>
 
 @implementation Bug
 
--setWorldSizeX: (int) xSize Y: (int) ySize {
+- setWorldSizeX: (int)xSize Y: (int)ySize
+{
   worldXSize = xSize;
   worldYSize = ySize;
   return self;
 }
 
--setFoodSpace: (id) f {
+- setFoodSpace: f
+{
    foodSpace = f;
    return self;
 }
 
--createEnd {
+- createEnd
+{
    [super createEnd];
    return self;
 }
 
--setX: (int) x Y: (int) y {
+- setX: (int)x Y: (int)y
+{
   xPos = x;
   yPos = y;
   printf("\n I started at X = %d Y = %d \n\n", xPos, yPos);
   return self;
 }
 
--step {
-
+- step
+{
   xPos = xPos + [uniformIntRand getIntegerWithMin: -1 withMax: 1];
   yPos = yPos + [uniformIntRand getIntegerWithMin: -1 withMax: 1];
 
   xPos = (xPos + worldXSize) % worldXSize;
   yPos = (yPos + worldYSize) % worldYSize;
 
-  if ([foodSpace getValueAtX: xPos Y: yPos] == 1) {
+  if ([foodSpace getValueAtX: xPos Y: yPos] == 1)
+    {
       [foodSpace putValue: 0 atX: xPos Y: yPos];
       printf( "I found food at X = %d Y = %d ! \n", xPos, yPos);
-  }
-
+    }
+  
   return self;
 }
   
--- apps/objc/tutorial/simpleSwarmBug/FoodSpace.m
+++ apps/objc/tutorial/simpleSwarmBug/FoodSpace.m
@@ -1,20 +1,20 @@
 // FoodSpace.m
 
 #import "FoodSpace.h"
-#import <simtools.h>
+#import <random.h>
 
 @implementation FoodSpace
 
--seedFoodWithProb: (float) seedProb {
+- seedFoodWithProb: (float)seedProb
+{
   int x,y;
-
+  
   for (y = 0; y < ysize; y++) 
     for (x = 0; x < xsize; x++) 
       if ([uniformDblRand getDoubleWithMin: 0.0 withMax: 1.0] < seedProb)
 	[self putValue: 1 atX: x Y: y];
-
+  
   return self;
-
 }
 
 @end
