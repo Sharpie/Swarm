f12c6475dcb968164b7c700e479d46cc3d083bd1
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1998-03-15 06:55:06 +0000

    * ILink.m: Don't Include tkobjc.h.  Reformatting throughout.
    * FNet.m: Don't include string.h, stdio.h, stdlib.h, or tkobjc.h.
    
    
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@738 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- apps/objc/bank/FNet.m
+++ apps/objc/bank/FNet.m
@@ -1,11 +1,3 @@
-#define __USE_FIXED_PROTOTYPES__  // for gcc headers
-
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-#import <tkobjc.h>
-
 #import "FNet.h"
 
 @implementation FNet
--- apps/objc/bank/ILink.m
+++ apps/objc/bank/ILink.m
@@ -1,24 +1,24 @@
-#define __USE_FIXED_PROTOTYPES__  // for gcc headers
-
-#import <tkobjc.h>
 #import "ILink.h"
 
 @implementation ILink
 
--createEnd {
-  [super createEnd] ;
+- createEnd
+{
+  [super createEnd];
   if(canvas) 
-    [linkItem setColor: "PaleGreen4"] ;
-  return self ;
+    [linkItem setColor: "PaleGreen4"];
+  return self;
 }
 
 
--(int) isInvestLink {
-  return 1 ;
+- (int)isInvestLink 
+{
+  return 1;
 }
 
--(int) isBorrowLink {
-  return 0 ;
+- (int)isBorrowLink
+{
+  return 0;
 }
 
 @end
