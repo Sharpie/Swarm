60213f3f1a03a267cfa7143add5998dd7e5960c0
alex <alex@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1998-03-19 01:38:28 +0000

    * Removed irrelevant import of <simtools.h>. Reformatting throughout.
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@794 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- apps/objc/tutorial/simpleExperBug/FoodSpace.h
+++ apps/objc/tutorial/simpleExperBug/FoodSpace.h
@@ -1,16 +1,15 @@
 // FoodSpace.h					simpleExperBug
 
 #import <space.h>
-#import <simtools.h>
 
-
-@interface FoodSpace : Discrete2d {
+@interface FoodSpace : Discrete2d 
+{
   int food;
 }
 
--(int)getFood;
--seedFoodWithProb: (float) s;
--bugAte;
+- (int)getFood;
+- seedFoodWithProb: (float)s;
+- bugAte;
 
 @end
 
