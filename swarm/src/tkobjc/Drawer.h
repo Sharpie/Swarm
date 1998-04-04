36958f87db3d0fd221116e0212f3296f75b1f6b9
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1998-04-04 21:46:45 +0000

    * Drawer.h:  Renamed from XDrawer.h.  Protocol is now consists of the method drawX:Y:raster:, not drawOn:X:Y:GC:Caller:.
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@829 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- swarm/src/tkobjc/Drawer.h
+++ swarm/src/tkobjc/Drawer.h
@@ -8,9 +8,7 @@
 // protocol for a "Drawer", something that draws itself with Xlib
 // calls on a Drawable at a given location. Raster calls this method
 // on objects it is given by the draw:X:Y: method.
-// Note that the GC is given to you just for convenience: you should
-// not permanently alter it, as other code might be using that same GC.
 
-@protocol XDrawer
-- drawOn: (Drawable)w X: (int)x Y: (int)y GC: (GC)gc Caller: Caller;
+@protocol Drawer
+- drawX: (int)x Y: (int)y raster: (Raster *)raster;
 @end
