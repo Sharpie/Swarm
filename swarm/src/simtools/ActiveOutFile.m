4587d907e9537734f4fcfce5abaeb36f683920b9
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1998-04-17 01:21:14 +0000

    Remove tagged documentation
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@865 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- swarm/src/simtools/ActiveOutFile.m
+++ swarm/src/simtools/ActiveOutFile.m
@@ -5,24 +5,15 @@
 
 #import <simtools/ActiveOutFile.h>
 #import <simtools/OutFile.h>
-#import <activity.h>
 
-//S: An object that actively updates its file stream when updated.
-
-//D: This is the file I/O equivalent of ActiveGraph: it takes an OutFile 
-//D: object, a target (datafeed) object, and a selector, which it uses to
-//D: extract data from the object and send it to the file. 
 @implementation ActiveOutFile
 
-//M: The setFileObject: method sets the file object to which the data will be
-//M: sent.
 - setFileObject: aFileObj
 {
   theFile = aFileObj;
   return self;
 }
 
-//M: The setDataFeed: method sets the object that will be probed for data.
 - setDataFeed: d
 {
   dataFeed = d;
@@ -38,8 +29,6 @@
   return self;
 }
 
-//M: The step method fires the probe, reads the value from the object, and 
-//M: sends the value to the file.
 - step
 {
   [theFile putDouble: [self doubleDynamicCallOn: dataFeed]];
