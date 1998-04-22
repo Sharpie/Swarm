a3d06a92e35a2d2a6f35b55aac3fbe0dbfb2c313
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1998-04-22 19:21:16 +0000

    * CommonProbeDisplay.h:  Include objectbase/SwarmObject.h, not   objectbase.h.
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@919 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- swarm/src/simtoolsgui/CommonProbeDisplay.h
+++ swarm/src/simtoolsgui/CommonProbeDisplay.h
@@ -3,7 +3,7 @@
 // implied warranty of merchantability or fitness for a particular purpose.
 // See file LICENSE for details and terms of copying.
 
-#import <objectbase.h>
+#import <objectbase/SwarmObject.h>
 #import <gui.h>
 
 @interface CommonProbeDisplay: SwarmObject
