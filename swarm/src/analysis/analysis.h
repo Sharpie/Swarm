87e7eacaf3d05466206f67b76b289390662d80c3
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1998-06-03 19:07:47 +0000

    * analysis.h: Updated documentation tags.
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@1276 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- swarm/src/analysis/analysis.h
+++ swarm/src/analysis/analysis.h
@@ -3,7 +3,7 @@
 // implied warranty of merchantability or fitness for a particular purpose.
 // See file LICENSE for details and terms of copying.
 
-//S: Analysis tools.
+//S: Analysis tools
 
 //D: This is the library where tools primarily related to analysis tasks,
 //D: reside. This includes tools which simplify the task of graphing
