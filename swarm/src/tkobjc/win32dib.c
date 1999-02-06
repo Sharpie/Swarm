2c4f241227c5a31e1ad06169e0b1342486c455eb
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1999-02-06 01:49:42 +0000

    Update copyright
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@2893 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- swarm/src/tkobjc/win32dib.c
+++ swarm/src/tkobjc/win32dib.c
@@ -1,3 +1,8 @@
+// Swarm library. Copyright (C) 1996-1999 Santa Fe Institute.
+// This library is distributed without any warranty; without even the
+// implied warranty of merchantability or fitness for a particular purpose.
+// See file LICENSE for details and terms of copying.
+
 #ifdef _WIN32
 #include "win32dib.h"
 #include <misc.h>
