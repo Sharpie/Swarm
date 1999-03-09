fc3bfecf29b02098cf2a882f5dd9165fd4cea18b
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1999-03-09 01:50:25 +0000

    * directory.h (JFINDOBJC): Formerly JFIND. (JFINDJAVA): New macro.
    * directory.c (compare_java_objects): Formerly compare_objects.
    (compare_objc_objects): New function.
    (java_tree): Formerly directory_tree.
    (objc_tree): New static variable.
    (java_directory_init): Initialize objc_tree.
    (java_directory_drop): Drop objc_tree.
    
    * directory.[hc] (java_directory_java_find): Formerly
    java_directory_find.
    (java_directory_objc_find): New function.
    
    
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@3147 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- swarm/java/c/directory.c
+++ swarm/java/c/directory.c
@@ -4,10 +4,11 @@
 #include <jni.h>
 #include <objc/objc.h>
 
-static avl_tree *directory_tree;
+static avl_tree *java_tree;
+static avl_tree *objc_tree;
 
 static int 
-compare_objects (const void *A, const void *B, void *PARAM)
+compare_java_objects (const void *A, const void *B, void *PARAM)
 {
   if (((jobject_id *) A)->java_object < ((jobject_id *) B)->java_object)
     return -1;
@@ -15,20 +16,42 @@ compare_objects (const void *A, const void *B, void *PARAM)
   return (((jobject_id *) A)->java_object > ((jobject_id *) B)->java_object);
 }
 
+static int 
+compare_objc_objects (const void *A, const void *B, void *PARAM)
+{
+  if (((jobject_id *) A)->objc_object < ((jobject_id *) B)->objc_object)
+    return -1;
+
+  return (((jobject_id *) A)->objc_object > ((jobject_id *) B)->objc_object);
+}
+
 jobject_id *
-java_directory_find (jobject java_object)
+java_directory_java_find (jobject java_object)
 {
   jobject_id pattern;
   jobject_id *result; 
   
   pattern.java_object = java_object;
-  result = avl_find (directory_tree, &pattern);
+  result = avl_find (java_tree, &pattern);
   if (!result) 
     abort ();
   
   return result;
 }
 
+jobject_id *
+java_directory_objc_find (id objc_object)
+{
+  jobject_id pattern;
+  jobject_id *result; 
+  
+  pattern.objc_object = objc_object;
+  result = avl_find (objc_tree, &pattern);
+  if (!result) 
+    abort ();
+  
+  return result;
+}
 
 jobject_id * 
 java_directory_add (jobject java_object, id objc_object)
@@ -39,18 +62,21 @@ java_directory_add (jobject java_object, id objc_object)
   data->objc_object = objc_object;
   data->java_object = java_object;
   
-  avl_insert (directory_tree, data);
+  avl_insert (java_tree, data);
+  avl_insert (objc_tree, data);
   return data;
 }
 
 void
 java_directory_init (void)
 {
-  directory_tree = avl_create (compare_objects, NULL);
+  java_tree = avl_create (compare_java_objects, NULL);
+  objc_tree = avl_create (compare_objc_objects, NULL);
 }
 
 void
 java_directory_drop (void)
 {
-  avl_free (directory_tree);
+  avl_destory (java_tree, NULL);
+  avl_free (objc_tree);
 }
--- swarm/java/c/directory.h
+++ swarm/java/c/directory.h
@@ -10,9 +10,11 @@ typedef struct jobject_id
 
 void java_directory_init (void);
 void java_directory_drop (void);
-jobject_id *java_directory_find (jobject java_object);
+jobject_id *java_directory_find_objc (jobject java_object);
+jobject_id *java_directory_find_java (id objc_object);
 jobject_id *java_directory_add (jobject java_object, id objc_object);
 
-#define JFIND(jobj) (java_directory_find (jobj))->objc_object
+#define JFINDOBJC(jobj) ((java_directory_find_objc (jobj))->objc_object)
+#define JFINDJAVA(obj) ((java_directory_find_java (obj))->java_object)
 #define JADD(jobj, objc) java_directory_add (jobj, objc);
 
