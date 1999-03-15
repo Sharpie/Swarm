7af54dedc4dfb452ada99e9657a1eccc72142647
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1999-03-15 00:27:21 +0000

    * Makefile.in (CPPFLAGS): Add -I for libobjc. (abs_top_builddir): New variable. (IMPMODULES): New variable. (libjavaswarm_la_LDFLAGS): Use it to get .libs directories for -L. Pass -L for libobjc/.libs.  Pass -rpath $(libdir). (libjavaswarm.la): Expand list of -l for IMPMODULES.
    * directory.h: fix names
    
    
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@3206 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- swarm/java/c/directory.h
+++ swarm/java/c/directory.h
@@ -10,12 +10,12 @@ typedef struct jobject_id
 
 void java_directory_init (void);
 void java_directory_drop (void);
-jobject_id *java_directory_find_objc (jobject java_object);
-jobject_id *java_directory_find_java (id objc_object);
+jobject_id *java_directory_java_find (jobject java_object);
+jobject_id *java_directory_objc_find (id objc_object);
 jobject_id *java_directory_add (jobject java_object, id objc_object);
 
-#define JFINDOBJC(jobj) ((java_directory_find_objc (jobj))->objc_object)
-#define JFINDJAVA(obj) ((java_directory_find_java (obj))->java_object)
+#define JFINDOBJC(jobj) ((java_directory_objc_find (jobj))->objc_object)
+#define JFINDJAVA(obj) ((java_directory_java_find (obj))->java_object)
 #define JADD(jobj, objc) java_directory_add (jobj, objc);
 
 SEL java_ensure_selector (JNIEnv *env, jobject jsel);
