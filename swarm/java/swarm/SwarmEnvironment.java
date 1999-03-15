e90d878d926b131baed569ce6d5c25ca8b063e41
vjojic <vjojic@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1999-03-15 21:32:39 +0000

    * SwarmEnvironment.java: Use System.loadLibrary instead of 	System.load
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@3211 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- swarm/java/swarm/SwarmEnvironment.java
+++ swarm/java/swarm/SwarmEnvironment.java
@@ -5,9 +5,8 @@ public class SwarmEnvironment
    static
     {
 	System.out.println("Trying to load lib!\n");
-	
 	try {
-	    System.load("/opt/src/vjojic/Swarm/swarm/java/swarm/libjavaswarm.so");
+	    System.loadLibrary("javaswarm");
 	} catch (Exception e) { 
 	    System.err.println("Exception caught: " + e.getMessage());
 	}
@@ -17,7 +16,6 @@ public class SwarmEnvironment
 
     public static void main(String args[])
     {
-	SwarmEnvironment se = new SwarmEnvironment();
 	initSwarm(args);
     }
     native static void initSwarm(String args[]);
