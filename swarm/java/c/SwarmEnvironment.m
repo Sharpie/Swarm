2b41b59cc1a79a54adbee6cb125587735ef1ff6d
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1999-03-17 00:08:27 +0000

    Tweaks for compilation
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@3241 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- swarm/java/c/SwarmEnvironment.m
+++ swarm/java/c/SwarmEnvironment.m
@@ -1,32 +1,31 @@
 #include "SwarmEnvironment.h"
-#include <directory.h>
-#include <stdio.h>
+#include <defobj/FCall.h>
+#include "directory.h"
+
 #include <misc.h>
 #include <simtools.h>
 
 void 
-Java_SwarmEnvironment_initSwarm(JNIEnv *env, jobject obj, jobjectArray args)
+Java_SwarmEnvironment_initSwarm (JNIEnv *env, jobject obj, jobjectArray args)
 {
+  int i = 0;
+  const char **argv;
+  int argc;
+  const char *utf;
+  jstring jstr;
+  jboolean isCopy;
 
-    jsize len; 
-    int i = 0;
-    char ** argv;
-    int argc;
-    const char * utf;
-    jstring jstr;
-    jboolean isCopy;
+  argc = (*env)->GetArrayLength (env, args);    
+  argv = (const char **) xmalloc (sizeof (char *) * argc);
 
-    argc = (*env)->GetArrayLength (env, args);    
-    argv = (char **) malloc (sizeof (char *) * len);
-
-    for (i=0; i<argc; i++)
-      {
-	jstr = (*env)->GetObjectArrayElement (env, args, i);
-	utf = (char *)(*env)->GetStringUTFChars (env, jstr, &isCopy);
-	argv[i] = isCopy ? (char *) utf : strdup (utf);
-      }
-    java_directory_init ();
-    init_javacall_tables ((void *) env);
-    initSwarm (argc, (const char **)argv);
+  for (i = 0; i < argc; i++)
+    {
+      jstr = (*env)->GetObjectArrayElement (env, args, i);
+      utf = (char *)(*env)->GetStringUTFChars (env, jstr, &isCopy);
+      argv[i] = isCopy ? (char *) utf : strdup (utf);
+    }
+  java_directory_init (env);
+  init_javacall_tables ((void *) env);
+  initSwarm (argc, (const char **)argv);
 }
 
