d62f90eb6333f43ee0189f0915fcd27c3ac60b27
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1999-03-24 20:51:01 +0000

    Reformatting
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@3380 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- swarm/java/swarm/Selector.java
+++ swarm/java/swarm/Selector.java
@@ -4,33 +4,33 @@ import swarm.SignatureNotFoundException;
 import java.lang.reflect.Method;
 
 public class Selector {
-    String signature;
-    Class retType;
-    Class [] argTypes;
-    boolean objcFlag;
-
+  String signature;
+  Class retType;
+  Class [] argTypes;
+  boolean objcFlag;
+  
   public Selector (Class theClass, String theMethodName, boolean theObjcFlag) throws NonUniqueMethodSignatureException, SignatureNotFoundException {
     Method[] methods = theClass.getMethods ();
     int matchCount = 0;
-
+    
     objcFlag = theObjcFlag;
     if (objcFlag)
-        theMethodName.replace (':', '$');
-
+      theMethodName.replace (':', '$');
+    
     for (int mi = 0; mi < methods.length; mi++)
-        {
-            if (methods[mi].getName ().compareTo (theMethodName) == 0)
-                {
-                    if (matchCount > 0)
-                        throw new NonUniqueMethodSignatureException ();
-                    signature = theMethodName;
-                    retType = methods[mi].getReturnType ();
-                    argTypes = methods[mi].getParameterTypes ();
-
-                    matchCount++;
-                }
-        }
+      {
+        if (methods[mi].getName ().compareTo (theMethodName) == 0)
+          {
+            if (matchCount > 0)
+              throw new NonUniqueMethodSignatureException ();
+            signature = theMethodName;
+            retType = methods[mi].getReturnType ();
+            argTypes = methods[mi].getParameterTypes ();
+            
+            matchCount++;
+          }
+      }
     if (matchCount != 1)
-        throw new SignatureNotFoundException ();
+      throw new SignatureNotFoundException ();
   }
 }
