7626633cfad30675f24efb6265d8ecee8d1b5148
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1999-04-25 00:54:07 +0000

    Formatting tweak
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@3540 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- swarm/src/defobj/Arguments.m
+++ swarm/src/defobj/Arguments.m
@@ -148,7 +148,7 @@ strip_quotes (const char *argv0)
 - createEnd
 {
   const char *argv0 = strip_quotes (argv[0]);
-  program_invocation_name = (char *)find_executable (argv0);
+  program_invocation_name = (char *) find_executable (argv0);
 #ifndef __GLIBC__
   program_invocation_short_name = getApplicationValue (argv0);
 #endif  
