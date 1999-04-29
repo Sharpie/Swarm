03534356fb2d9e768776298632a16809845e58f3
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1999-04-29 00:50:27 +0000

    * mframe.m: Declare alloca.
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@3622 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- swarm/libobjc/mframe.m
+++ swarm/libobjc/mframe.m
@@ -54,6 +54,8 @@ BOOL sel_types_match (const char* t1, const char* t2);
 /* memory.h and strings.h conflict on some systems.  */
 #endif /* not STDC_HEADERS and not HAVE_STRING_H */
 
+void *alloca (size_t);
+
 
 /* For encoding and decoding the method arguments, we have to know where
    to find things in the "argframe" as returned by __builtin_apply_args.
