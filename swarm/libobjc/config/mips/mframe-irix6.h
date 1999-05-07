68c5d64d07abc216a4b4e13e6ad23c465ac933ed
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1999-05-07 23:58:11 +0000

    Syntax errors
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@3769 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- swarm/libobjc/config/mips/mframe-irix6.h
+++ swarm/libobjc/config/mips/mframe-irix6.h
@@ -4,11 +4,11 @@
 #define MFRAME_ARGS_SIZE 136
 #define MFRAME_RESULT_SIZE 16 
 
-inline static
-void **mframe_get_struct_addr_ptr (arglist_t args, const char *types)
+inline static void **
+mframe_get_struct_addr_ptr (arglist_t args, const char *types)
 {
   return (*types ==_C_ARY_B) ?
-    (method_get_next_argument (ARGS, &types))
+    (void **) (method_get_next_argument (args, &types))
     : (((*types == _C_STRUCT_B || *types ==_C_UNION_B)
         && objc_sizeof_type (types) > MFRAME_SMALL_STRUCT)
        ? ((void **) args + 1)
@@ -17,7 +17,7 @@ void **mframe_get_struct_addr_ptr (arglist_t args, const char *types)
     
 #define MFRAME_GET_STRUCT_ADDR(ARGS, TYPES) \
 ({ void **ptr = mframe_get_struct_addr_ptr (ARGS, TYPES); \
-   ptr ? *ptr : NULL })
+   ptr ? *ptr : NULL; })
 
 #define MFRAME_SET_STRUCT_ADDR(ARGS, TYPES, ADDR) \
 ( { void **ptr = mframe_get_struct_addr_ptr (ARGS, TYPES); \
