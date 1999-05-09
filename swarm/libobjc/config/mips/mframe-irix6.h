893a761f310518623dde4dd947a93a84fc78bf52
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1999-05-09 18:54:44 +0000

    *** empty log message ***
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@3779 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- swarm/libobjc/config/mips/mframe-irix6.h
+++ swarm/libobjc/config/mips/mframe-irix6.h
@@ -3,19 +3,12 @@
 #define MFRAME_STRUCT_BYREF 0
 #define MFRAME_ARGS_SIZE 136
 #define MFRAME_RESULT_SIZE 16 
-#define MFRAME_FLT_IN_FRAME_AS_DBL 1
 
 inline static BOOL
 refstructp (const char *types)
 {
   return ((*types == _C_STRUCT_B || *types ==_C_UNION_B)
           && objc_sizeof_type (types) > MFRAME_SMALL_STRUCT);
-}
-
-inline static BOOL
-floatp (const char *types)
-{
-  return *types == _C_FLT || *types == _C_DBL;
 }
 
 inline static void **
@@ -35,16 +28,18 @@ mframe_get_struct_addr_ptr (arglist_t args, const char *types)
     if (ptr) *ptr = (ADDR); } )
 
 struct mips_args {
-  int reg_offset;
-  int float_reg_offset;
+  unsigned reg_offset;
+  unsigned float_reg_offset;
+  unsigned stack_offset;
 };
      
 #define MFRAME_ARGS struct mips_args
 
 #define MFRAME_INIT_ARGS(CUM, RTYPE) \
 ({ \
- (CUM).reg_offset = 20; \
- (CUM).float_reg_offset = 88; \
+ (CUM).reg_offset = 0; \
+ (CUM).float_reg_offset = 0; \
+ (CUM).stack_offset = 96; \
 })
 
 #define MFRAME_ARG_ENCODING(CUM, TYPE, STACK, DEST) \
@@ -52,30 +47,64 @@ struct mips_args {
   const char* type = (TYPE); \
   unsigned align = objc_alignof_type (type); \
   unsigned size = objc_sizeof_type (type); \
+  BOOL float_flag = *type == _C_FLT; \
+  BOOL double_flag = *type == _C_DBL; \
   BOOL structref_flag = refstructp (type); \
-  BOOL float_flag = floatp (type); \
-  unsigned offset = structref_flag ? 4 \
-                    : float_flag ? (CUM).float_reg_offset : (CUM).reg_offset; \
+  unsigned offset; \
+  BOOL register_flag = NO; \
   \
-  offset = ROUND (offset, align); \
   (TYPE) = objc_skip_typespec (type); \
   \
+  if (*(TYPE) == '+') \
+    register_flag = YES; \
+  if (!structref_flag) \
+    { \
+      if (register_flag) \
+        { \
+          offset = atoi (TYPE); \
+          if (float_flag || double_flag) \
+            { \
+              if (float_flag) \
+                offset += 4; \
+              offset = ROUND (offset, align); \
+              (CUM).float_reg_offset = offset; \
+            } \
+          else \
+            { \
+              offset += 4; \
+              offset = ROUND (offset, align); \
+              (CUM).reg_offset = offset; \
+            } \
+        } \
+      else \
+        { \
+          offset = (CUM).stack_offset; \
+          offset = ROUND (offset, align); \
+        } \
+    } \
+  else \
+    { \
+      offset = (CUM).reg_offset; \
+      register_flag = YES; \
+      offset = ROUND (offset, 8); \
+    } \
+  sprintf ((DEST), "%.*s%s%d", ((TYPE) - type), type, \
+	   register_flag ? "+" : "", offset); \
   if (*(TYPE) == '+') \
     { \
       (TYPE)++; \
     } \
-  sprintf ((DEST), "%.*s%d", (int) ((TYPE)-type), type, offset); \
   while (isdigit ((int) *(TYPE))) \
     { \
       (TYPE)++; \
     } \
   (DEST)=&(DEST)[strlen (DEST)]; \
-  if (!structref_flag) \
+  if (register_flag) \
     { \
-      if (float_flag) \
-        (CUM).float_reg_offset = offset + size; \
-      else \
-        (CUM).reg_offset = offset + size; \
+      (CUM).float_reg_offset += size; \
+      (CUM).reg_offset += size; \
     } \
+  else \
+    (CUM).stack_offset += size; \
 })
 
