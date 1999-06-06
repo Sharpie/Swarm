76d6e5a331ecffcf20ce62eeed1e155ebdfad055
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1999-06-06 17:33:37 +0000

    Comment tweak
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@4191 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- swarm/src/simtoolsgui/ControlPanel.m
+++ swarm/src/simtoolsgui/ControlPanel.m
@@ -134,7 +134,7 @@ PHASE(Using)
       [self setState: ControlStateStopped];
       [ctlObj waitRun];
 #endif
-      // Check now if the user hit the quit button: if so, abort.
+      // Check now if the user hit the quit button: if so, quit.
       if (state == ControlStateQuit)
         exit(0);
       else
