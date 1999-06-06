d42a79b5cd97517dbc8fc0a1f1a061eb6173506f
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1999-06-06 17:31:21 +0000

    * include.dists.creating.m: Reformatting throughout. Use of raiseEvent macro instead of raiseEvent: method.
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@4190 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- swarm/src/random/include.dists.creating.m
+++ swarm/src/random/include.dists.creating.m
@@ -5,35 +5,43 @@
 // 
 
 // Use if the generator is a Simple one:
--setGenerator: (id) generator {
-
+- setGenerator: generator
+{
    if (randomGenerator) 
-   [InvalidCombination raiseEvent:
-   "%s: setting the generator more than once not allowed\n",distName];
-
+     raiseEvent (InvalidCombination, 
+                 "%s: setting the generator more than once not allowed\n",
+                 distName);
+   
    randomGenerator = generator;
    generatorMax = [randomGenerator getUnsignedMax];
+   
+   if ([randomGenerator respondsTo: M(advanceAll)])
+     { 
+       // split
+       useSplitGenerator = YES;
+       // printf("%s setGenerator: using split generator %s\n",
+       // distName, [randomGenerator getName]);
+       
+     }
+   else if ([randomGenerator respondsTo: M(getUnsignedSample)])
+     {
+       // simple
+       
+       useSplitGenerator = NO;
+       virtualGenerator = 0;
+       // printf("%s setGenerator: using simple generator %s\n",
+       // distName, [randomGenerator getName]);
+       
+     }
+   else
+     {
+       // neither
 
-   if ([randomGenerator respondsTo: M(advanceAll)])               { // split
-
-     useSplitGenerator = YES;
-	// printf("%s setGenerator: using split generator %s\n",
-	// distName, [randomGenerator getName]);
-
-   } else if ([randomGenerator respondsTo: M(getUnsignedSample)]) { // simple
-
-     useSplitGenerator = NO;
-     virtualGenerator = 0;
-	// printf("%s setGenerator: using simple generator %s\n",
-	// distName, [randomGenerator getName]);
-
-   } else {							     // neither
-
-     [InvalidCombination raiseEvent:
-     "%s setGenerator: not a generator !!!\n",distName];
-
-   }
-
+       raiseEvent (InvalidCombination,
+                   "%s setGenerator: not a generator !!!\n",
+                   distName);
+     }
+   
 /*
 // This code does not work, for unknown reasons:
    if ( [randomGenerator conformsTo: @protocol(SimpleOut)]) {
@@ -43,88 +51,101 @@
 */
 
    if (useSplitGenerator) 
-   [InvalidCombination raiseEvent:
-   "%s: wrong version of create: split generator %s detected\n",
-   distName, [randomGenerator getName]];
-
+     raiseEvent (InvalidCombination,
+                 "%s: wrong version of create: split generator %s detected\n",
+                 distName, [randomGenerator getName]);
+   
    [self resetState];
-
+   
    return self;
 }
 
 // Use this if the generator is a Split one:
--setGenerator: (id) generator setVirtualGenerator: (unsigned) vGen {
-   unsigned k;
-
-   if (randomGenerator) 
-   [InvalidCombination raiseEvent:
-   "%s: setting the generator more than once not allowed\n",distName];
-
-   randomGenerator = generator;
-   generatorMax = [randomGenerator getUnsignedMax];
-
-   if ([randomGenerator respondsTo: M(advanceAll)])               { // split
-
-     useSplitGenerator = YES;
-	// printf("%s setGenerator: using split generator %s\n",
-	// distName, [randomGenerator getName]);
-
-   } else if ([randomGenerator respondsTo: M(getUnsignedSample)]) { // simple
-
-     useSplitGenerator = NO;
-     virtualGenerator = 0;
-	// printf("%s setGenerator: using simple generator %s\n",
-	// distName, [randomGenerator getName]);
-
-   } else {							     // neither
-
-     [InvalidCombination raiseEvent:
-     "%s setGenerator: not a generator !!!\n",distName];
-
+- setGenerator: generator setVirtualGenerator: (unsigned)vGen
+{
+  unsigned k;
+  
+  if (randomGenerator) 
+    raiseEvent (InvalidCombination,
+                "%s: setting the generator more than once not allowed\n",
+                distName);
+  
+  randomGenerator = generator;
+  generatorMax = [randomGenerator getUnsignedMax];
+  
+  if ([randomGenerator respondsTo: M(advanceAll)])
+    {
+      // split
+      
+      useSplitGenerator = YES;
+      // printf("%s setGenerator: using split generator %s\n",
+      // distName, [randomGenerator getName]);
+      
+    }
+  else if ([randomGenerator respondsTo: M(getUnsignedSample)])
+    {
+      // simple
+      
+      useSplitGenerator = NO;
+      virtualGenerator = 0;
+      // printf("%s setGenerator: using simple generator %s\n",
+      // distName, [randomGenerator getName]);
+      
+    }
+  else
+    {	
+      // neither
+      
+      raiseEvent (InvalidCombination,
+                  "%s setGenerator: not a generator !!!\n",
+                  distName);
+      
    }
-
-/*
-// This code does not work, for unknown reasons:
-   if ( [randomGenerator conformsTo: @protocol(SimpleOut)]) {
-   } else if ( [randomGenerator conformsTo: @protocol(SplitOut)]) {
-   } else {
-   }
-*/
-
-   if (!useSplitGenerator) 
-      [InvalidCombination raiseEvent:
-      "%s: wrong version of create: non-split generator %s detected\n",
-      distName, [randomGenerator getName]];
-
-   if (virtualGenerator != MAXVGEN) 
-      [InvalidCombination raiseEvent:
-      "%s setVirtualGenerator: already set\n", distName];
-
-   k = [randomGenerator getNumGenerators];
-
-   if (vGen >= k)
-      [InvalidCombination raiseEvent:
-      "%s setVirtualGenerator: vGen=%d > generator allows (%d)\n", 
-      vGen, k-1];
-
-   virtualGenerator = vGen;
-
-   [self resetState];
-
-   return self;
+  
+  /*
+    // This code does not work, for unknown reasons:
+    if ( [randomGenerator conformsTo: @protocol(SimpleOut)]) {
+    } else if ( [randomGenerator conformsTo: @protocol(SplitOut)]) {
+    } else {
+    }
+  */
+  
+  if (!useSplitGenerator) 
+    raiseEvent (InvalidCombination,
+                "%s: wrong version of create: non-split generator %s detected\n",
+                distName, [randomGenerator getName]);
+  
+  if (virtualGenerator != MAXVGEN) 
+    raiseEvent (InvalidCombination
+                "%s setVirtualGenerator: already set\n",
+                distName);
+  
+  k = [randomGenerator getNumGenerators];
+  
+  if (vGen >= k)
+    raiseEvent (InvalidCombination,
+                "%s setVirtualGenerator: vGen=%d > generator allows (%d)\n", 
+                vGen, k - 1];
+  
+  virtualGenerator = vGen;
+  
+  [self resetState];
+  
+  return self;
 }
 
 
--createEnd {
-
-// If no generator has been allocated, abort:
-
-   if (!randomGenerator)	
-      [InvalidCombination raiseEvent:
-      "%s initialized without a generator!\n",
-      distName];
-
+- createEnd
+{
+  // If no generator has been allocated, abort:
+  
+  if (!randomGenerator)	
+    raiseEvent (InvalidCombination, 
+                "%s initialized without a generator!\n",
+                distName);
+  
    return [super createEnd];
 }
 
 // dists.include.creating.m
+
