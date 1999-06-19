51c0ff6cfc6e0a21b530a80c40362f11ccb12dec
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1999-06-19 19:24:00 +0000

    * simtools.m (run_constructors): New static function. (_initSwarm_): Call it for hpux.
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@4388 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- swarm/src/simtools/simtools.m
+++ swarm/src/simtools/simtools.m
@@ -15,6 +15,42 @@
 
 externvardef BOOL swarmGUIMode = NO;
 
+static void
+run_constructors (void)
+{
+  extern void libobjc_sl_0_constructor (void);
+  extern void libmisc_sl_2_constructor (void);
+  extern void libdefobj_sl_2_constructor (void);
+  extern void libcollections_sl_2_constructor (void);
+  extern void libactivity_sl_3_constructor (void);
+  extern void libobjectbase_sl_3_constructor (void);
+  extern void librandom_sl_1_constructor (void);
+  extern void libtclobjc_sl_1_constructor (void);
+  extern void libtkobjc_sl_4_constructor (void);
+  extern void libsimtools_sl_3_constructor (void);
+  extern void libsimtoolsgui_sl_4_constructor (void);
+  extern void libanalysis_sl_3_constructor (void);
+  extern void libspace_sl_2_constructor (void);
+  extern void libswarm_sl_0_constructor (void);
+
+  printf ("running constructors\n");
+  libobjc_sl_0_constructor ();
+  libmisc_sl_2_constructor ();
+  libdefobj_sl_2_constructor ();
+  libcollections_sl_2_constructor ();
+  libactivity_sl_3_constructor ();
+  libobjectbase_sl_3_constructor ();
+  librandom_sl_1_constructor ();
+  libtclobjc_sl_1_constructor ();
+  libtkobjc_sl_4_constructor ();
+  libsimtools_sl_3_constructor ();
+  libsimtoolsgui_sl_4_constructor ();
+  libanalysis_sl_3_constructor ();
+  libspace_sl_2_constructor ();
+  libswarm_sl_0_constructor ();
+}
+#endif 
+
 void
 _initSwarm_ (int argc, const char **argv, const char *appName,
       const char *version, const char *bugAddress,
@@ -23,6 +59,9 @@ _initSwarm_ (int argc, const char **argv, const char *appName,
       int (*optionFunc) (int key, const char *arg),
       BOOL forceBatchMode)
 {
+#ifdef hpux
+  run_constructors ();
+#endif
   initModule (activity);
 
   initDefobj (argc, argv, 
@@ -40,3 +79,4 @@ _initSwarm_ (int argc, const char **argv, const char *appName,
   if (swarmGUIMode)
     initSimtoolsGUI ();
 }
+
