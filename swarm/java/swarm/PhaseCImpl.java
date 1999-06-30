4913e0e82cfae11807161f4cb1d6e00410b52b0e
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1999-06-30 01:26:20 +0000

    *** empty log message ***
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@4500 84d386b0-d280-4341-8980-c4cb7e1eb7a5

---
+++ swarm/java/swarm/PhaseCImpl.java
@@ -0,0 +1,11 @@
+package swarm;
+public class PhaseCImpl {
+  public Object nextPhase;
+  public PhaseCImpl () {
+    super ();
+  }
+  public PhaseCImpl (Object nextPhase) {
+    super ();
+    this.nextPhase = nextPhase;
+  }
+}
