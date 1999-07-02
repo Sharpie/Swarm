0583fffccf7cd9be83fd5f2160d61249f0c9b321
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1999-07-02 19:27:42 +0000

    Undo
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@4557 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- swarm/libobjc/mframe.m
+++ swarm/libobjc/mframe.m
@@ -1728,9 +1728,7 @@ mframe_decode_return (const char *type, void* buffer, void* retframe)
 	{
 	  __builtin_return (rframe);
 	}
-#if 0
 	*(block*)buffer = retframe_block(retframe);
-#endif
 	break;
       }
 
