7ce1ce016d0bcfdcbc020aa4ed7f1acf0f778ed1
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1999-07-05 19:22:02 +0000

    * dropdir.c (SLASHP): New macro. (dropdir): Use it.
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@4583 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- swarm/src/misc/dropdir.c
+++ swarm/src/misc/dropdir.c
@@ -1,5 +1,7 @@
 #include <misc.h>
 
+#define SLASHP(c) ((c) == '\\' || (c) == '/')
+
 char *
 dropdir (char *path)
 {
@@ -8,14 +10,14 @@ dropdir (char *path)
       unsigned start_len = strlen (path), len;
       
       if (start_len > 0)
-	if (path[start_len - 1] == '/')
+	if (SLASHP (path[start_len - 1]))
 	  start_len--;
       
       for (len = start_len; len > 0; len--)
 	{
 	  char *ptr = &path[len - 1];
 	  
-	  if (*ptr == '/' && start_len > 1)
+	  if (SLASHP (*ptr) && start_len > 1)
 	    {
 	      ptr++;
 	      *ptr = '\0';
