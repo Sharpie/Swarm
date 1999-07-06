d629331474b38c5b57e8afb2614499c142df2745
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1999-07-06 19:57:59 +0000

    * fcall_{objc,java}.[hm]: New files.
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@4599 84d386b0-d280-4341-8980-c4cb7e1eb7a5

---
+++ swarm/src/defobj/fcall_java.h
@@ -0,0 +1,15 @@
+#import <defobj/FArguments.h>
+#import <defobj/FCall.h>
+#include <jni.h>
+
+extern void java_setup_call (FArguments_c *fa,
+                             JNIEnv *env,
+                             jobject obj,
+                             jmethodID method);
+extern void java_setup_static_call (FArguments_c *fa,
+                                    JNIEnv *env,
+                                    jclass class,
+                                    jmethodID method);
+extern void java_add_primitive (FArguments_c *fa, fcall_type_t type, void *val);
+extern void java_set_return_type (FCall_c *fc, FArguments_c *fa);
+extern void java_call (FArguments_c *fa);
---
+++ swarm/src/defobj/fcall_java.m
@@ -0,0 +1,32 @@
+#import "fcall_java.h"
+#import <defobj/FArguments.h>
+
+#ifdef __CYGWIN__
+#define MSVC
+#endif
+#include <avcall.h>
+
+void
+java_setup_call (FArguments_c *fa,
+                 JNIEnv *env,
+                 jobject obj,
+                 jmethodID method)
+{
+  av_ptr (fa->java_avalist, JNIEnv *, env);
+  av_ptr (fa->java_avalist, jobject, obj);
+  av_ptr (fa->avalist, jmethodID, method);
+}
+
+void
+java_setup_static_call (FArguments *fa,
+                        JNIEnv *env,
+                        jclass class,
+                        jmethodID method)
+{
+  av_ptr (fa->java_avalist, JNIEnv *, env);
+  av_ptr (fa->java_avalist, jclass, class);
+  av_ptr (fa->avalist, jmethodID, method);
+}
+
+#define JAVA
+#include "_fcall.m"
