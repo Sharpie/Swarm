d6ed825cd322cbd95d07125a454d23c5ea1ae78b
vjojic <vjojic@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1999-07-12 21:55:33 +0000

    * SwarmEnvironment.java: New getCurrent... methods that correspond 	to calls of getCurrent... macros from activity.h
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@4648 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- swarm/java/swarm/SwarmEnvironment.java
+++ swarm/java/swarm/SwarmEnvironment.java
@@ -2,6 +2,8 @@ package swarm;
 import swarm.random.*;
 import swarm.simtoolsgui.*;
 import swarm.defobj.*;
+import swarm.activity.*;
+import swarm.objectbase.*;
 
 public class SwarmEnvironment {
   static {
@@ -15,6 +17,15 @@ public class SwarmEnvironment {
     System.out.println ("Lib loaded!\n");
   }
   public native void initSwarm (String args[]);
+  public native int getCurrentTime ();
+  public native SwarmImpl getCurrentSwarm ();
+  public native ScheduleImpl getCurrentSchedule ();
+  public native SwarmActivityImpl getCurrentSwarmActivity ();
+  public native ScheduleActivityImpl getCurrentScheduleActivity();
+  public native ActivityImpl getCurrentOwnerActivity ();
+  public native ActionImpl getCurrentAction ();
+  public native ActivityImpl getCurrentActivity();
+
   public ZoneImpl globalZone;
   public UniformIntegerDistImpl uniformIntRand;
   public UniformDoubleDistImpl uniformDblRand;
