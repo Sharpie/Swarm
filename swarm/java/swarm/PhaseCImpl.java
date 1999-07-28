16d8d00ff9143db27ca2044a26e55ffe54e5b3b8
vjojic <vjojic@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1999-07-28 00:51:32 +0000

    * PhaseCImpl.java (_copy_creating_phase_to_using_phase): 	New function that copies values of variables from creating 	phase object to using phase object.
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@4726 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- swarm/java/swarm/PhaseCImpl.java
+++ swarm/java/swarm/PhaseCImpl.java
@@ -1,6 +1,39 @@
 package swarm;
+import java.lang.reflect.*;
 public class PhaseCImpl {
   public Object nextPhase;
+  public void _copy_creating_phase_to_using_phase ()
+  {
+     Field[] creating_phase_fields;
+     Field using_phase_field;
+     Class nextPhaseClass;
+     int i = 0;
+     nextPhaseClass = nextPhase.getClass ();
+     creating_phase_fields = this.getClass().getDeclaredFields();
+     
+     try {
+       for (i=0; i<creating_phase_fields.length; i++)
+	 {
+	   System.out.println (creating_phase_fields[i].getName());
+	   using_phase_field = 
+	     nextPhaseClass.getDeclaredField(creating_phase_fields[i].getName());
+	   
+	   if (using_phase_field != null)
+	     using_phase_field.set(nextPhase, 
+				   creating_phase_fields[i].get(this));
+	 }
+     } catch (Exception e) 
+       { 
+	 System.out.println ("\nWhile copying field: " +
+			     creating_phase_fields[i].getName() +
+			     "\nfrom an instance of class: " + 
+			     this.getClass().getName() +
+			     "\nto an instance of class: " + 
+			     nextPhase.getClass().getName() +
+			     "\ngot exception: " + e.toString () + "\n");
+       }
+  }
+
   public PhaseCImpl () {
     super ();
   }
