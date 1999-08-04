0e586ae39142874a13b85987bec9ec577f827e25
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1999-08-04 20:17:02 +0000

    (Selector): Allow duplicate methods to be found, but check that the types are the same.
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@4786 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- swarm/java/swarm/Selector.java
+++ swarm/java/swarm/Selector.java
@@ -25,15 +25,32 @@ public class Selector {
         {
           if (methods[mi].getName ().compareTo (theMethodName) == 0)
             {
-              if (matchCount > 0)
-                throw new NonUniqueMethodSignatureException ();
+              if (matchCount > 0) {
+                if (signature != theMethodName)
+                  System.err.println ("signature: " + signature + " != " + theMethodName);
+                {
+                  int i;
+                  Class []margTypes = methods[mi].getParameterTypes ();
+                  
+                  if (margTypes.length != argTypes.length)
+                    throw new NonUniqueMethodSignatureException ();
+
+                  for (i = 0; i < argTypes.length; i++)
+                    if (argTypes[i] != margTypes[i]) {
+                      System.err.println (signature + " arg: " + i + ": " + argTypes[i] + " != " + margTypes[i]);
+                      throw new NonUniqueMethodSignatureException ();
+                    }
+                }
+                if (retType != methods[mi].getReturnType ())
+                  System.err.println (signature + " retType: "+ retType + " != " + methods[mi].getReturnType ());
+              }
               signature = theMethodName;
               retType = methods[mi].getReturnType ();
               argTypes = methods[mi].getParameterTypes ();
               matchCount++;
             }
         }
-      if (matchCount != 1)
+      if (matchCount == 0)
         throw new SignatureNotFoundException ();
     }
   }
