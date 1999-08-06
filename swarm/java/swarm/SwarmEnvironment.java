081d760aa7100bbf78bdd0c90e1320c68b803b2d
alex <alex@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1999-08-06 01:45:58 +0000

    * Makefile.in (POSTCLASSES): Add Globals.
    * Globals.java: New class.  Make `env' a public static variable of
    type `SwarmEnvironment'.
    
    * SwarmEnvironment.java: Move the static initialization of the
    javaswarm library to new Globals class.
    
    
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@4806 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- swarm/java/swarm/SwarmEnvironment.java
+++ swarm/java/swarm/SwarmEnvironment.java
@@ -6,48 +6,34 @@ import swarm.activity.*;
 import swarm.objectbase.*;
 
 public class SwarmEnvironment {
-  static {
-    System.out.println ("Trying to load lib!\n");
-    try {
-      System.loadLibrary ("javaswarm");
-    }
-    catch (Exception e) {
-      System.err.println ("Exception caught: " + e.getMessage ());
-    } 
-    System.out.println ("Lib loaded!\n");
-  }
-  public native void initSwarm (String args[]);
-  public native int getCurrentTime ();
-  public native SwarmImpl getCurrentSwarm ();
-  public native ScheduleImpl getCurrentSchedule ();
-  public native SwarmActivityImpl getCurrentSwarmActivity ();
-  public native ScheduleActivityImpl getCurrentScheduleActivity();
-  public native ActivityImpl getCurrentOwnerActivity ();
-  public native ActionImpl getCurrentAction ();
-  public native ActivityImpl getCurrentActivity();
 
-  public native Object createProbeDisplay (Object anOjbect);
-  public native Object createCompleteProbeDisplay (Object anObject);
-  public native Object createArchivedProbeDisplay (Object anObject);
-  public native Object createArchivedCompleteProbeDisplay (Object anObject);
-  public native Object setWindowGeometryRecordName (Object theWidget);
-  public native Object setComponentWindowGeometryRecordNameFor (Object obj,
-							      Object widget);
-  public native Object setComponentWindowGeometryRecordName (Object widget);
+    public native void initSwarm (String args[]);
+    public native int getCurrentTime ();
+    public native SwarmImpl getCurrentSwarm ();
+    public native ScheduleImpl getCurrentSchedule ();
+    public native SwarmActivityImpl getCurrentSwarmActivity ();
+    public native ScheduleActivityImpl getCurrentScheduleActivity();
+    public native ActivityImpl getCurrentOwnerActivity ();
+    public native ActionImpl getCurrentAction ();
+    public native ActivityImpl getCurrentActivity();
+    
+    public native Object createProbeDisplay (Object anOjbect);
+    public native Object createCompleteProbeDisplay (Object anObject);
+    public native Object createArchivedProbeDisplay (Object anObject);
+    public native Object createArchivedCompleteProbeDisplay (Object anObject);
+    public native Object setWindowGeometryRecordName (Object theWidget);
+    public native Object setComponentWindowGeometryRecordNameFor (Object obj, Object widget);
+    public native Object setComponentWindowGeometryRecordName (Object widget);
 
-  
-  public SymbolImpl ControlStateRunning, ControlStateStopped,
-    ControlStateStepping, ControlStateQuit, ControlStateNextTime;
-  public ProbeLibraryImpl probeLibrary;
-  public ProbeDisplayManagerImpl probeDisplayManager;
-
-  public ZoneImpl globalZone;
-  public UniformIntegerDistImpl uniformIntRand;
-  public UniformDoubleDistImpl uniformDblRand;
-  public SwarmEnvironment (String args[]) {
-    initSwarm (args);
-
-  }
+    public  SymbolImpl ControlStateRunning, ControlStateStopped,
+        ControlStateStepping, ControlStateQuit, ControlStateNextTime;
+    public  ProbeLibraryImpl probeLibrary;
+    public  ProbeDisplayManagerImpl probeDisplayManager;
+    
+    public  ZoneImpl globalZone;
+    public  UniformIntegerDistImpl uniformIntRand;
+    public  UniformDoubleDistImpl uniformDblRand;
+    
 }
 
 
