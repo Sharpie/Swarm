381b9d33797562d7af1d31f356fd6735ced7ad01
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1999-08-10 00:45:59 +0000

    * SwarmEnvironment.java (SwarmEnvironment): New variable guiFlag.
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@4901 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- swarm/java/swarm/SwarmEnvironment.java
+++ swarm/java/swarm/SwarmEnvironment.java
@@ -6,34 +6,33 @@ import swarm.activity.*;
 import swarm.objectbase.*;
 
 public class SwarmEnvironment {
-
-    public native void initSwarm (String args[]);
-    public native int getCurrentTime ();
-    public native SwarmImpl getCurrentSwarm ();
-    public native ScheduleImpl getCurrentSchedule ();
-    public native SwarmActivityImpl getCurrentSwarmActivity ();
-    public native ScheduleActivityImpl getCurrentScheduleActivity();
-    public native ActivityImpl getCurrentOwnerActivity ();
-    public native ActionImpl getCurrentAction ();
-    public native ActivityImpl getCurrentActivity();
-    
-    public native Object createProbeDisplay (Object anOjbect);
-    public native Object createCompleteProbeDisplay (Object anObject);
-    public native Object createArchivedProbeDisplay (Object anObject);
-    public native Object createArchivedCompleteProbeDisplay (Object anObject);
-    public native Object setWindowGeometryRecordName (Object theWidget);
-    public native Object setComponentWindowGeometryRecordNameFor (Object obj, Object widget);
-    public native Object setComponentWindowGeometryRecordName (Object widget);
-
-    public  SymbolImpl ControlStateRunning, ControlStateStopped,
-        ControlStateStepping, ControlStateQuit, ControlStateNextTime;
-    public  ProbeLibraryImpl probeLibrary;
-    public  ProbeDisplayManagerImpl probeDisplayManager;
-    
-    public  ZoneImpl globalZone;
-    public  UniformIntegerDistImpl uniformIntRand;
-    public  UniformDoubleDistImpl uniformDblRand;
-    
+  public native void initSwarm (String args[]);
+  public native int getCurrentTime ();
+  public native SwarmImpl getCurrentSwarm ();
+  public native ScheduleImpl getCurrentSchedule ();
+  public native SwarmActivityImpl getCurrentSwarmActivity ();
+  public native ScheduleActivityImpl getCurrentScheduleActivity();
+  public native ActivityImpl getCurrentOwnerActivity ();
+  public native ActionImpl getCurrentAction ();
+  public native ActivityImpl getCurrentActivity();
+  
+  public native Object createProbeDisplay (Object anOjbect);
+  public native Object createCompleteProbeDisplay (Object anObject);
+  public native Object createArchivedProbeDisplay (Object anObject);
+  public native Object createArchivedCompleteProbeDisplay (Object anObject);
+  public native Object setWindowGeometryRecordName (Object theWidget);
+  public native Object setComponentWindowGeometryRecordNameFor (Object obj, Object widget);
+  public native Object setComponentWindowGeometryRecordName (Object widget);
+  
+  public SymbolImpl ControlStateRunning, ControlStateStopped,
+    ControlStateStepping, ControlStateQuit, ControlStateNextTime;
+  public ProbeLibraryImpl probeLibrary;
+  public ProbeDisplayManagerImpl probeDisplayManager;
+  
+  public ZoneImpl globalZone;
+  public UniformIntegerDistImpl uniformIntRand;
+  public UniformDoubleDistImpl uniformDblRand;
+  public boolean guiFlag;
 }
 
 
