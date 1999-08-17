eef3157e9c05c8703b60b0e73c308bc95d61bb02
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1999-08-17 19:04:21 +0000

    Split out StartMousetrap class into new file (JBuilder wants it this way)
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@4968 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- apps/java/jmousetrap/Mousetrap.java
+++ apps/java/jmousetrap/Mousetrap.java
@@ -115,40 +115,6 @@ public class Mousetrap
 }
 
 
-/**
- * This class is the `main' function of the Mousetrap application */
-class StartMousetrap
-{
-  public static void nag (String s)
-  {
-    System.out.println ( s);
-  }
-  public static void main (String[] args)
-  {
-    Globals.env.initSwarm(args);
-
-    if (Globals.env.guiFlag) {
-        
-      MousetrapObserverSwarm topLevelSwarm 
-        = new MousetrapObserverSwarm(Globals.env.globalZone);
-        
-      Globals.env.setWindowGeometryRecordName (topLevelSwarm);
-      topLevelSwarm.buildObjects ();
-      topLevelSwarm.buildActions ();
-      topLevelSwarm.activateIn (null);
-      topLevelSwarm.go ();
-        
-    }
-    else {
-      MousetrapBatchSwarm topLevelSwarm 
-        = new MousetrapBatchSwarm((ZoneImpl)Globals.env.globalZone);
-      topLevelSwarm.buildObjects();
-      topLevelSwarm.buildActions();
-      topLevelSwarm.activateIn(null);
-      topLevelSwarm.go();
-    }
-  }
-}
 
 
 
