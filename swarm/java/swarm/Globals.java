0563af86de6716b54df4e58e4f7335f37b41aae8
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1999-08-18 16:09:53 +0000

    minor reformatting
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@4972 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- swarm/java/swarm/Globals.java
+++ swarm/java/swarm/Globals.java
@@ -2,22 +2,21 @@ package swarm;
 import swarm.*;
 
 final public class Globals {
+  public static SwarmEnvironment env;
 
-    public static SwarmEnvironment env;
-  
-    static {
-        System.out.println ("Trying to load lib!\n");
-        try {
-            System.loadLibrary ("javaswarm");
-        }
-        catch (Exception e) {
-            System.err.println ("Exception caught: " + e.getMessage ());
-        } 
-        System.out.println ("Lib loaded!\n");
-        env = new SwarmEnvironment();
+  static {
+    System.out.println ("Trying to load lib!\n");
+    try {
+      System.loadLibrary ("javaswarm");
     }
+    catch (Exception e) {
+      System.err.println ("Exception caught: " + e.getMessage ());
+    } 
+    System.out.println ("Lib loaded!\n");
     
-    private Globals() { /* not instantiatiable by user */ 
-        
-    }
+    env = new SwarmEnvironment ();
+  }
+  
+  private Globals () { /* not instantiatiable by user */ 
+  }
 }
