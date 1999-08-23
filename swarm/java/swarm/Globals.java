a177646e677e6ae906962a22503619eb53b2df14
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1999-08-23 19:22:38 +0000

    * Globals.java: Remove loading messages.
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@5025 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- swarm/java/swarm/Globals.java
+++ swarm/java/swarm/Globals.java
@@ -5,15 +5,12 @@ final public class Globals {
   public static SwarmEnvironment env;
 
   static {
-    System.out.println ("Trying to load lib!\n");
     try {
       System.loadLibrary ("javaswarm");
     }
     catch (Exception e) {
       System.err.println ("Exception caught: " + e.getMessage ());
     } 
-    System.out.println ("Lib loaded!\n");
-    
     env = new SwarmEnvironment ();
   }
   
