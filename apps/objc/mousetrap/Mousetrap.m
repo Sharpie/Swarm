1b1ccb726d46807e3feaaca099f87286d2b2fc70
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1999-08-27 00:44:34 +0000

    Update copyright
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@5118 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- apps/objc/mousetrap/Mousetrap.m
+++ apps/objc/mousetrap/Mousetrap.m
@@ -1,4 +1,4 @@
-// Mousetraps application. Copyright (C) 1996-1999 Santa Fe Institute.
+// Mousetraps application. Copyright © 1996-1999 Santa Fe Institute.
 // This library is distributed without any warranty; without even the
 // implied warranty of merchantability or fitness for a particular purpose.
 // See file LICENSE for details and terms of copying.
