8d713942340b004fdb0e89f5a2f3572dece56837
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1999-09-13 01:22:30 +0000

    * SwarmEnvironment.m: Don't include misc.h. (Java_swarm_SwarmEnvironment_createArchivedProbeDisplay, Java_swarm_SwarmEnvironment_createArchivedCompleteProbeDisplay, Java_swarm_SwarmEnvironment_setWindowGeometryRecordName, Java_swarm_SwarmEnvironment_setComponentWindowGeometryRecordNameFor, Java_swarm_SwarmEnvironment_setComponentWindowGeometryRecordName): Add window geometry record name arguments. (Java_swarm_SwarmEnvironment_initSwarm): Add appName, version, bugAddress arguments; init via _initSwarm_.
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@5293 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- swarm/java/c/SwarmEnvironment.m
+++ swarm/java/c/SwarmEnvironment.m
@@ -1,7 +1,6 @@
 // include first to avoid Kaffe jmalloc/stdlib conflict
-#include <misc.h> // xmalloc, strdup
 #import <simtools.h> // initSwarm, swarmGUIMode
-#import <defobj.h> // defobj_java_call_init_tables
+#import <defobj.h> // defobj_java_call_init_tables, SSTRDUP
 #import "directory.h" // java_directory_init, JAVA_APPNAME
 #import <simtoolsgui.h> // probe display macros
 
@@ -58,14 +57,15 @@ Java_swarm_SwarmEnvironment_getCurrentTime (JNIEnv *env, jobject obj)
 }
 
 JNIEXPORT jobject JNICALL 
-Java_swarm_SwarmEnvironment_createProbeDisplay (JNIEnv * env, jobject obj, 
+Java_swarm_SwarmEnvironment_createProbeDisplay (JNIEnv *env,
+                                                jobject obj, 
 						jobject anObject)
 {
   return SD_ENSUREJAVA (env, CREATE_PROBE_DISPLAY (SD_FINDOBJC (env, anObject)));
 }
 
 JNIEXPORT jobject JNICALL 
-Java_swarm_SwarmEnvironment_createCompleteProbeDisplay (JNIEnv * env, 
+Java_swarm_SwarmEnvironment_createCompleteProbeDisplay (JNIEnv *env, 
 							jobject obj, 
 							jobject anObject)
 {
@@ -75,48 +75,97 @@ Java_swarm_SwarmEnvironment_createCompleteProbeDisplay (JNIEnv * env,
 JNIEXPORT jobject JNICALL 
 Java_swarm_SwarmEnvironment_createArchivedProbeDisplay (JNIEnv * env, 
 							jobject obj, 
-							jobject anObject)
+							jobject anObject,
+                                                        jstring objectName)
 {
-  return SD_ENSUREJAVA (env, CREATE_ARCHIVED_PROBE_DISPLAY (SD_FINDOBJC (env, anObject)));
+  jboolean isCopy;
+  jobject probeDisplay;
+
+  const char *name = (*env)->GetStringUTFChars (env, objectName, &isCopy);
+  
+  probeDisplay =
+    SD_ENSUREJAVA (env,
+                   createArchivedProbeDisplayNamed
+                   (SD_FINDOBJC (env, anObject), name));
+  (*env)->ReleaseStringUTFChars (env, objectName, name);
+  return probeDisplay;
 }
 
 
 JNIEXPORT jobject JNICALL 
 Java_swarm_SwarmEnvironment_createArchivedCompleteProbeDisplay (JNIEnv * env, 
-							   jobject obj, 
-							   jobject anObject)
+                                                                jobject obj, 
+                                                                jobject anObject,
+                                                                jstring objectName)
 {
-  return 
-    SD_ENSUREJAVA (env, CREATE_ARCHIVED_COMPLETE_PROBE_DISPLAY (SD_FINDOBJC (env, anObject)));
+  jboolean isCopy;
+  jobject probeDisplay;
+
+  const char *name = (*env)->GetStringUTFChars (env, objectName, &isCopy);
+
+  probeDisplay =
+    SD_ENSUREJAVA (env,
+                   createArchivedCompleteProbeDisplayNamed
+                   (SD_FINDOBJC (env, anObject), name));
+  
+  (*env)->ReleaseStringUTFChars (env, objectName, name);
+  return probeDisplay;
 }
 
 
 JNIEXPORT jobject JNICALL 
-Java_swarm_SwarmEnvironment_setWindowGeometryRecordName (JNIEnv * env, 
+Java_swarm_SwarmEnvironment_setWindowGeometryRecordName (JNIEnv *env, 
 							 jobject obj, 
-							 jobject anObject)
+							 jobject anObject,
+                                                         jstring objectName)
 {
-  return 
-    SD_ENSUREJAVA (env, SET_WINDOW_GEOMETRY_RECORD_NAME (SD_FINDOBJC (env, anObject)));
+  jboolean isCopy;
+  const char *name = (*env)->GetStringUTFChars (env, objectName, &isCopy);
+  jobject ret;
+
+  ret =
+    SD_ENSUREJAVA (env,
+                   [SD_FINDOBJC (env, anObject) setWindowGeometryRecordName: name]);
+  if (isCopy)
+    (*env)->ReleaseStringUTFChars (env, objectName, name);
+  return ret;
 }
 
 JNIEXPORT jobject JNICALL 
-Java_swarm_SwarmEnvironment_setComponentWindowGeometryRecordNameFor (JNIEnv * env, jobject obj, jobject anObj, jobject widget)
+Java_swarm_SwarmEnvironment_setComponentWindowGeometryRecordNameFor (JNIEnv *env,
+                                                                     jobject obj,
+                                                                     jobject anObj,
+                                                                     jobject widget,
+                                                                     jobject widgetName)
 {
-  return SD_ENSUREJAVA (env, SET_COMPONENT_WINDOW_GEOMETRY_RECORD_NAME_FOR (SD_FINDOBJC (env, anObj), SD_FINDOBJC (env, widget)));
+  id objcObject = SD_FINDOBJC (env, anObj);
+  id objcWidget = SD_FINDOBJC (env, widget);
+  jboolean isCopy;
+  const char *name = (*env)->GetStringUTFChars (env, widgetName, &isCopy);
+  jobject ret =
+    SD_ENSUREJAVA (env, 
+                   [objcObject setWindowGeometryRecordNameForComponent: 
+                                 name widget: objcWidget]);
+  if (isCopy)
+    (*env)->ReleaseStringUTFChars (env, widgetName, name);
+  return ret;
 }
 
 JNIEXPORT jobject JNICALL 
-Java_swarm_SwarmEnvironment_setComponentWindowGeometryRecordName (JNIEnv * env,
+Java_swarm_SwarmEnvironment_setComponentWindowGeometryRecordName (JNIEnv *env,
 								  jobject obj,
-								  jobject anObj)
-{
-  return SD_ENSUREJAVA (env, SET_COMPONENT_WINDOW_GEOMETRY_RECORD_NAME_FOR (SD_FINDOBJC (env, obj), SD_FINDOBJC (env, anObj)));
+								  jobject widget,
+                                                                  jobject widgetName)
+{ 
+  return Java_swarm_SwarmEnvironment_setComponentWindowGeometryRecordNameFor (env, obj, obj, widget, widgetName);
 }
 
 JNIEXPORT void JNICALL
 Java_swarm_SwarmEnvironment_initSwarm (JNIEnv *env,
                                        jobject obj,
+                                       jstring appName,
+                                       jstring version,
+                                       jstring bugAddress,
                                        jobjectArray args)
 {
   int i = 0;
@@ -129,17 +178,20 @@ Java_swarm_SwarmEnvironment_initSwarm (JNIEnv *env,
   argc = (*env)->GetArrayLength (env, args) + 1;   
   argv = (const char **) xmalloc (sizeof (const char *) * argc);
 
-  argv[0] = "javaswarm";
+  argv[0] = (*env)->GetStringUTFChars (env, appName, &isCopy);
   for (i = 0; i < argc - 1; i++)
     {
       jstr = (*env)->GetObjectArrayElement (env, args, i);
       utf = (const char *) (*env)->GetStringUTFChars (env, jstr, &isCopy);
-      argv[i + 1] = isCopy ? (const char *) utf : strdup (utf);
+      argv[i + 1] = isCopy ? (const char *) utf : SSTRDUP (utf);
     }
 
   defobj_init_java_call_tables ((void *) env);
-#define APPNAME JAVA_APPNAME
-  initSwarmApp (argc, argv, VERSION, "bug-swarm@santafe.edu");
+  _initSwarm_ (argc, argv,
+               argv[0],
+               (*env)->GetStringUTFChars (env, version, &isCopy),
+               (*env)->GetStringUTFChars (env, bugAddress, &isCopy),
+               NULL, NULL, NULL, NO);
 #ifdef hpux
   {
     extern void libjavaswarmstubs_constructor (void);
