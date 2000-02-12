c28f6ee1fc88b67637097ef19c30be2b6257ebe8
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
2000-02-12 02:27:13 +0000

    Reformatting of copyright
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@5726 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- swarm/src/random/RWC8gen.h
+++ swarm/src/random/RWC8gen.h
@@ -1,7 +1,7 @@
-// Swarm library. Copyright © 1996-2000 Swarm Development Group. This library is
-//   distributed without any warranty; without even the implied warranty
-//   of merchantability or fitness for a particular purpose.
-// See file LICENSE for details and terms of copying.
+// Swarm library. Copyright © 1996-2000 Swarm Development Group.
+// This library is distributed without any warranty; without even the
+// implied warranty of merchantability or fitness for a particular
+// purpose.  See file LICENSE for details and terms of copying.
 
 /*
 Name:            RWC8gen.h
