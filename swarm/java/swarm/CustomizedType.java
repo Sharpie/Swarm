f8dacd74cf87acd0f1cf5326a2fc786cff9aac10
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
2000-02-18 23:25:17 +0000

    * Makefile.in (POSTCLASSES): Add CustomizedType.
    * CustomizedType.java: New file.
    
    
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@5776 84d386b0-d280-4341-8980-c4cb7e1eb7a5

---
+++ swarm/java/swarm/CustomizedType.java
@@ -0,0 +1,12 @@
+// Swarm library. Copyright © 2000 Swarm Development Group.
+// This library is distributed without any warranty; without even the
+// implied warranty of merchantability or fitness for a particular purpose.
+// See file LICENSE for details and terms of copying.
+
+package swarm;
+
+public class CustomizedType {
+  public CustomizedType () {
+    super ();
+  }
+}
