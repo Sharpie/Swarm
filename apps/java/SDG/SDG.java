19560570d9cfc6483d5da327e70afdc85aa2d868
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
2000-05-31 20:07:32 +0000

    *** empty log message ***
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@6067 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- apps/java/SDG/SDG.java
+++ apps/java/SDG/SDG.java
@@ -1,28 +1,19 @@
-import com.sun.j3d.utils.applet.MainFrame;
-import javax.media.j3d.BranchGroup;
+import javax.media.j3d.TransformGroup;
+
 import swarm.Globals;
+import swarm.defobj.Zone;
 
 public class SDG extends Organization {
-  public BranchGroup createSceneGraph () {
-    BranchGroup root = super.createSceneGraph ();
+  public SDG (Zone aZone, TransformGroup group) {
+    super (aZone, group);
+
     Agent mgd = new Marcus ();
     Agent gepr = new Glen ();
-
+    
     addAgent (mgd);
     addAgent (gepr);
     
     gepr.moveAgent (0.0, -0.25, 0.25);
     mgd.step ();
-
-    return root;
-  }
-
-  public SDG (String[] args) {
-    super (args);
-  }
-
-  public static void main (String[] args) {
-    Globals.env.initSwarm ("SDG", "0.0", "bug-swarm@swarm.org", args);
-    new MainFrame (new SDG (args), 200, 200);
   }
 }
---
+++ apps/java/SDG/StartSDG.java
@@ -0,0 +1,81 @@
+import java.applet.Applet;
+import com.sun.j3d.utils.applet.MainFrame;
+import com.sun.j3d.utils.universe.SimpleUniverse;
+import com.sun.j3d.utils.behaviors.mouse.MouseRotate;
+import com.sun.j3d.utils.behaviors.mouse.MouseTranslate;
+import com.sun.j3d.utils.behaviors.mouse.MouseZoom;
+
+import javax.media.j3d.Background;
+import javax.media.j3d.BoundingSphere;
+import javax.media.j3d.BranchGroup;
+import javax.media.j3d.Canvas3D;
+import javax.media.j3d.TransformGroup;
+import javax.vecmath.Color3f;
+import javax.vecmath.Point3d;
+import javax.vecmath.Point3f;
+import java.awt.BorderLayout;
+import java.awt.GraphicsConfiguration;
+
+import swarm.Globals;
+
+public class StartSDG extends Applet {
+  TransformGroup rootTrans;
+  public StartSDG () {
+  }
+
+  public BranchGroup createSceneGraph () {
+    BranchGroup root = new BranchGroup ();
+    BoundingSphere bounds =
+      new BoundingSphere (new Point3d (0.0, 0.0, 0.0), 100.0);
+
+    rootTrans = new TransformGroup ();
+
+    rootTrans.setCapability (TransformGroup.ALLOW_TRANSFORM_WRITE);
+    rootTrans.setCapability (TransformGroup.ALLOW_TRANSFORM_READ);
+    
+    MouseRotate behavior = new MouseRotate ();
+    behavior.setTransformGroup (rootTrans);
+    rootTrans.addChild (behavior);
+    behavior.setSchedulingBounds (bounds);
+    
+    MouseZoom behavior2 = new MouseZoom ();
+    behavior2.setTransformGroup (rootTrans);
+    rootTrans.addChild (behavior2);
+    behavior2.setSchedulingBounds (bounds);
+    
+    // Create the translate behavior node
+    MouseTranslate behavior3 = new MouseTranslate();
+    behavior3.setTransformGroup(rootTrans);
+    rootTrans.addChild (behavior3);
+    behavior3.setSchedulingBounds (bounds);
+
+    // Set up the background
+    Color3f bgColor = new Color3f (0.05f, 0.05f, 0.5f);
+    Background bgNode = new Background (bgColor);
+    bgNode.setApplicationBounds (bounds);
+    rootTrans.addChild (bgNode);
+
+    root.addChild (rootTrans);
+    return root;
+  }
+
+  public void init () {
+    setLayout (new BorderLayout ());
+    GraphicsConfiguration config = SimpleUniverse.getPreferredConfiguration ();
+    Canvas3D c = new Canvas3D (config);
+    add ("Center", c);
+
+    BranchGroup scene = createSceneGraph ();
+
+    new SDG (Globals.env.globalZone, rootTrans);
+
+    SimpleUniverse u = new SimpleUniverse (c);
+    u.getViewingPlatform ().setNominalViewingTransform ();
+    u.addBranchGraph (scene);
+  }
+
+  public static void main (String[] args) {
+    Globals.env.initSwarm ("SDG", "0.0", "bug-swarm@swarm.org", args);
+    new MainFrame (new StartSDG (), 200, 200);
+  }
+}
