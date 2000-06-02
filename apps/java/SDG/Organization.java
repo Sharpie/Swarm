1998ceee39b5a4ee11f9c3530fafc2697de35d8d
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
2000-06-02 00:22:58 +0000

    *** empty log message ***
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@6077 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- apps/java/SDG/Organization.java
+++ apps/java/SDG/Organization.java
@@ -1,8 +1,6 @@
 import swarm.objectbase.SwarmImpl;
 import swarm.defobj.Zone;
 
-import agent3d.Agent3d;
-
 public class Organization extends SwarmImpl {
   public Organization (Zone aZone) {
     super (aZone);
