a200fb1b00b3f20cc7fad98dc01b6f7ba520db10
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
2000-07-20 17:23:21 +0000

    * Organization.java (reaper, getReaper, Organization, activateIn): Configure reaper queue.
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@6295 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- apps/java/SDG/Organization.java
+++ apps/java/SDG/Organization.java
@@ -3,18 +3,38 @@ import swarm.defobj.Zone;
 import swarm.space.Grid2d;
 import swarm.space.Grid2dImpl;
 
+import swarm.activity.Schedule;
+import swarm.activity.ScheduleC;
+import swarm.activity.ScheduleImpl;
+import swarm.activity.ScheduleCImpl;
+import swarm.activity.Activity;
+import swarm.objectbase.Swarm;
+
 public abstract class Organization extends SwarmImpl {
   Grid2d world;
   int xsize, ysize;
+  Schedule reaper;
   
   public Organization (Zone aZone, int xsize, int ysize) {
     super (aZone);
     this.xsize = xsize;
     this.ysize = ysize;
     world = new Grid2dImpl (getZone (), xsize, ysize);
+    reaper = new ScheduleImpl (aZone);
   }
 
   public Grid2d getWorld () {
     return world;
+  }
+
+  public Schedule getReaper () {
+    return reaper;
+  }
+
+  public Activity activateIn (Swarm swarmContext) {
+    super.activateIn (swarmContext);
+    
+    reaper.activateIn (this);
+    return getActivity ();
   }
 }
