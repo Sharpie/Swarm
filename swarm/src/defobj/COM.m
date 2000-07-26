101c58d739942cd2868f3f4dd48696f8cf9571d3
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
2000-07-26 21:04:58 +0000

    * Makefile.am (SRC): Add COM.m.
            * COM.m: New file.
    
    
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@6333 84d386b0-d280-4341-8980-c4cb7e1eb7a5

---
+++ swarm/src/defobj/COM.m
@@ -0,0 +1,33 @@
+#include <defobj/COM.h>
+
+COMclass
+swarm_directory_objc_find_COM_class (Class clazz)
+{
+  abort ();
+}
+
+COMobject
+swarm_directory_objc_ensure_COM (id object)
+{
+  abort ();
+}
+
+id
+swarm_directory_COM_ensure_objc (COMobject cobj)
+{
+  abort ();
+}
+
+SEL
+swarm_directory_COM_ensure_selector (COMobject csel)
+{
+  abort ();
+}
+
+Class
+swarm_directroy_COM_ensure_Class (COMclass clazz)
+{
+  abort ();
+}
+
+
