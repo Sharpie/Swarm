ef3841af8c97837d9ffc7ef0180f5cf499b152b6
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
2000-08-14 23:15:21 +0000

    Add COMsupport.cpp to CPPSRCS and fix INCLUDES path
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@6357 84d386b0-d280-4341-8980-c4cb7e1eb7a5

---
+++ swarm/COM/c/COMsupport.cpp
@@ -0,0 +1,76 @@
+#include "nsISupports.h"
+#include "nsIInterfaceInfo.h"
+#include "nsIInterfaceInfoManager.h"
+#include "nsIEnumerator.h"
+#include "nsMemory.h"
+#include "xptinfo.h"
+#include "COM.h"
+#include "COMsupport.h"
+
+extern "C" {
+
+void *
+findInterface (COMEnv *env, const char *name)
+{
+  nsIInterfaceInfoManager *iim = nsnull;
+  nsIEnumerator *Interfaces = nsnull;
+  nsISupports *is_Interface;
+  nsIInterfaceInfo *Interface;
+  nsresult rv;
+
+  if (!(iim = XPTI_GetInterfaceInfoManager ()))
+    {
+      NS_ASSERTION (0, "failed to get the InterfaceInfoManager");
+      goto done;
+    }
+  
+  if (NS_FAILED (iim->EnumerateInterfaces (&Interfaces)))
+    {
+      NS_ASSERTION (0, "failed to get interface enumeration");
+      goto done;
+    }
+  
+  if (NS_FAILED (rv = Interfaces->First()))
+    {
+      NS_ASSERTION (0, "failed to go to first item in interface enumeration");
+      goto done;
+    }
+  
+  do
+    {
+      if (NS_FAILED (rv = Interfaces->CurrentItem (&is_Interface)))
+        {
+          /* maybe something should be done,
+           * debugging info at least? */
+          Interfaces->Next ();
+          continue;
+        }
+      
+      rv = is_Interface->
+        QueryInterface (NS_GET_IID (nsIInterfaceInfo),
+                        (void **) &Interface);
+      
+      if (!NS_FAILED (rv))
+        {
+          char *interface_name;
+
+          Interface->GetName (&interface_name);
+          printf ("Got: [%s]\n", interface_name);
+          nsMemory::Free (interface_name);
+          NS_RELEASE (Interface);
+        }
+      else
+        abort ();
+      NS_RELEASE (is_Interface);
+      Interfaces->Next();
+      
+    } while (NS_COMFALSE == Interfaces->IsDone ());
+  
+ done:
+  NS_IF_RELEASE (Interfaces);
+  NS_IF_RELEASE (iim);
+
+  return NULL;
+}
+
+}
---
+++ swarm/COM/c/COMsupport.h
@@ -0,0 +1,5 @@
+extern "C" {
+
+void *findInterface (COMEnv *env, const char *name);
+
+}
