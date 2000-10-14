2d01ee20b2ecf2eef7296faf9b0298b69abdf67f
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
2000-10-14 20:18:31 +0000

    * defobj.h (DefinedObject): Add -conformsTo:.
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@6534 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- swarm/src/defobj/defobj.h
+++ swarm/src/defobj/defobj.h
@@ -110,6 +110,7 @@ USING
 - (BOOL)respondsTo: (SEL)aSel;
 
 + (BOOL)conformsTo: (Protocol *)protocol;
+- (BOOL)conformsTo: (Protocol *)protocol;
 
 //M: getClass returns the class that implements the current behavior of an
 //M: object.
