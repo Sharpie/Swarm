3b1b2528392fdd7fd6cd2908d7c31b3f9c72048e
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
2001-01-21 21:54:39 +0000

    COMmethodArgFcallType -> COMmethodParamFcallType
    Add COMmethodSetReturn
    
    
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@6626 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- swarm/COM/COM.el
+++ swarm/COM/COM.el
@@ -648,7 +648,7 @@
   (insert "swarmSwarmEnvironmentImpl::Init ()\n")
   (insert "{\n")
   
-  (insert "  static COMEnv env = { createComponent, findComponent, copyString, getName, getComponentName, copyComponentID, normalize, selectorQuery, selectorIsJavaScript, selectorIsVoidReturn, selectorIsBooleanReturn, selectorName, selectorArgCount, selectorArgFcallType, selectorCOMInvoke, selectorJSInvoke, COMcreateParams, COMsetArg, COMsetReturn, COMfreeParams, JScreateParams, JSsetArg, JSsetReturn, JSfreeParams, COMcollect, COMmethodName, COMmethodArgCount, COMmethodArgFcallType, COMmethodInvoke };\n")
+  (insert "  static COMEnv env = { createComponent, findComponent, copyString, getName, getComponentName, copyComponentID, normalize, selectorQuery, selectorIsJavaScript, selectorIsVoidReturn, selectorIsBooleanReturn, selectorName, selectorArgCount, selectorArgFcallType, selectorCOMInvoke, selectorJSInvoke, COMcreateParams, COMsetArg, COMsetReturn, COMfreeParams, JScreateParams, JSsetArg, JSsetReturn, JSfreeParams, COMcollect, COMmethodName, COMmethodArgCount, COMmethodParamFcallType, COMmethodSetReturn, COMmethodInvoke };\n")
   (insert "  initCOM (&env);\n")
   (insert "  return NS_OK;\n")
   (insert "}\n\n"))
