5f2fd6e7b5bf8e1dcb90e11d0c9c894083727b28
mgd <mgd@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
2003-05-02 16:30:22 +0000

    * DefObject.m ([Object_s -dropAllocations:]): Use DROP again; remove baseDrop lookup.
    * macros.h: Enable method functions by default, but use the new
    computed swarm_* function pointers instead of compiler symbols.
    
    
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@7370 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- swarm/src/defobj/macros.h
+++ swarm/src/defobj/macros.h
@@ -1,21 +1,21 @@
-#ifdef METHOD_FUNCTIONS
+#if 1
 struct Zone_c;
 struct Object_s;
 struct ComponentZone_c;
 
-extern id _i_Zone_c__allocIVarsComponent_ (struct Zone_c *, struct objc_selector *, Class);
-extern void _i_Zone_c__freeIVarsComponent_ (struct Zone_c *, struct objc_selector *, id);
-extern void *_i_Zone_c__allocBlock_ (struct Zone_c *, struct objc_selector *, size_t);
-extern void _i_Zone_c__freeBlock_blockSize_ (struct Zone_c *, struct objc_selector *, void *, size_t);
-extern id _i_ComponentZone_c__allocIVars_ (struct ComponentZone_c *, struct objc_selector *, Class);
+extern id (*_swarm_i_Zone_c__allocIVarsComponent_) (struct Zone_c *, struct objc_selector *, Class);
+extern void (*_swarm_i_Zone_c__freeIVarsComponent_) (struct Zone_c *, struct objc_selector *, id);
+extern void * (*_swarm_i_Zone_c__allocBlock_) (struct Zone_c *, struct objc_selector *, size_t);
+extern void (*_swarm_i_Zone_c__freeBlock_blockSize_) (struct Zone_c *, struct objc_selector *, void *, size_t);
+extern id (*_swarm_i_ComponentZone_c__allocIVars_) (struct ComponentZone_c *, struct objc_selector *, Class);
+extern id (*_swarm_i_Object_s__drop) (struct Object_s *, struct objc_selector *);
 
-#define ALLOCIVARSCOMPONENT(zone, class) _i_Zone_c__allocIVarsComponent_ (zone, M(allocIVarsComponent:), class)
-#define FREEIVARSCOMPONENT(zone, obj) _i_Zone_c__freeIVarsComponent_ ((struct Zone_c *) zone, M(freeIVarsComponent:), obj)
-#define COMPONENT_ALLOCIVARS(zone,class) _i_ComponentZone_c__allocIVars_ ((struct ComponentZone_c *) getCZone (zone), M(allocIVars:), class)
-#define ALLOCBLOCK(zone, size) _i_Zone_c__allocBlock_ ((struct Zone_c *) zone, M(allocBlock:), size)
-#define FREEBLOCK_SIZE(zone, ptr, size) _i_Zone_c__freeBlock_blockSize_ ((struct Zone_c *) zone, M(freeBlock:blockSize:), ptr, size);
-#define DROP(obj) _i_Object_s__drop (obj, M(drop)) 
-
+#define ALLOCIVARSCOMPONENT(zone, class) _swarm_i_Zone_c__allocIVarsComponent_ (zone, M(allocIVarsComponent:), class)
+#define FREEIVARSCOMPONENT(zone, obj) _swarm_i_Zone_c__freeIVarsComponent_ ((struct Zone_c *) zone, M(freeIVarsComponent:), obj)
+#define COMPONENT_ALLOCIVARS(zone,class) _swarm_i_ComponentZone_c__allocIVars_ ((struct ComponentZone_c *) getCZone (zone), M(allocIVars:), class)
+#define ALLOCBLOCK(zone, size) _swarm_i_Zone_c__allocBlock_ ((struct Zone_c *) zone, M(allocBlock:), size)
+#define FREEBLOCK_SIZE(zone, ptr, size) _swarm_i_Zone_c__freeBlock_blockSize_ ((struct Zone_c *) zone, M(freeBlock:blockSize:), ptr, size);
+#define DROP(obj) _swarm_i_Object_s__drop (obj, M(drop)) 
 
 #else
 #define ALLOCIVARSCOMPONENT(zone, class) [zone allocIVarsComponent: class]
