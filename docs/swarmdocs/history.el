ed7c1908d6cece0dd3d9c6b7d549f01c48afa149
alexl <alexl@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
2003-09-08 10:02:49 +0000

    * history.el (process-changelog): Use correct module-sym values: "refbook", "overbook", "installbook".
    * global.ent: Bump GFDL version to 1.2.  Don't exercise options,
    i.e. "no Invariant Sections, Back or Front Cover Texts".
    
    * configure.in: Bump version date to 2003-09-08.
    
    * gfdl.xml: Update to use GFDL 1.2 from the FSF:
    http://www.gnu.org/licenses/fdl.xml.
    
    
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@7458 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- docs/swarmdocs/history.el
+++ docs/swarmdocs/history.el
@@ -155,7 +155,7 @@
                          module-arg
                          (intern (car (last command-line-args)))))
          (swarmsrcdir-changelog-list
-          (if (member module-sym '(refbook tech set over install))
+          (if (member module-sym '(refbook tech set overbook installbook))
               nil
               (parse-changelog (pathname-for-module-sym module-sym "ChangeLog")
                                (header-filename-for-module-sym module-sym))))
