565afef8daf5f99c17369d73d74438c638d280a9
northcob <northcob@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
2003-09-16 01:01:45 +0000

    updates to build with Apple Project Builder.
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@7483 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- swarm/macosx/swarmx/configure.sh
+++ swarm/macosx/swarmx/configure.sh
@@ -4,14 +4,23 @@ cd $BUILT_PRODUCTS_DIR/
 
 export MACOSX_DEPLOYMENT_TARGET=10.2
 
-../../../configure \
-CC=/usr/local/gcc3.3/bin/gcc \
---prefix=/usr/local/swarm2.1 \
---with-tcldir=/usr/local/tcl8.4.1 \
---with-tkdir=/usr/local/tk8.4.1 \
+# Edit this configure command to reflect local 
+# library installation paths.
+# Optimise for G4 or G5 by adding stuff like -fast
+# -mcpuG5 -mtuneG5 etc to CFLAGS. (not tested!)
+
+../../../configure CC=cc \
+CFLAGS="-ggdb -O2 -Wno-long-double" \
+--prefix=/usr/local/swarm2.2p \
+--with-tcldir=/usr/local/tcl8.4.4 \
+--with-tkdir=/usr/local/tk8.4.4 \
 --with-bltdir=/usr/local/blt2.4z \
 --with-pngdir=/usr/local/png_1.2.5 \
---without-jdkdir \
---disable-static \
+--with-hdf5dir=/usr/local/hdf5_1.4.5p2 \
+--with-jdkdir=/System/Library/Frameworks/JavaVM.framework \
 --disable-fast-install \
---with-hdf5dir=/usr/local/hdf5_1.4.5
+--enable-onelib \
+--disable-static 
+
+# --disable-onelib \
+# --without-jdkdir \
