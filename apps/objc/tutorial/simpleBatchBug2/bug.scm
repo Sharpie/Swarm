9f7a14aacc08cf4970f87a8b5515516c561b5b28
pauljohn32 <pauljohn32@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
2004-04-13 21:54:58 +0000

    bug.scm is necessary for this model.
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@7517 84d386b0-d280-4341-8980-c4cb7e1eb7a5

---
+++ apps/objc/tutorial/simpleBatchBug2/bug.scm
@@ -0,0 +1,7 @@
+(list 
+ (cons 'modelSwarm
+       (make-instance 'ModelSwarm
+                      #:worldXSize 80
+                      #:worldYSize 80
+                      #:seedProb 1.0F0
+                      #:bugDensity 0.02F0)))
