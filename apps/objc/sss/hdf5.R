ed3468c0c651815b1e13963bcc5a6fa429a5a7fd
pauljohn32 <pauljohn32@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
2005-02-15 02:23:09 +0000

    Now R and hdf5 work for me, so I can properly create sss.hdf
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@7674 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- apps/objc/sss/hdf5.R
+++ apps/objc/sss/hdf5.R
@@ -1,13 +1,14 @@
-# Read the values as integers (reals are the default).
+library(hdf5)
+## Read the values as integers (reals are the default).
 v <- scan ("sugarspace.pgm", what=integer(0), skip=3)
 
-# Construct a matrix out of the vector, by telling it the number
-# of rows and that it should load row major order (by default it does
-# column major like FORTRAN).
+## Construct a matrix out of the vector, by telling it the number
+## of rows and that it should load row major order (by default it does
+## column major like FORTRAN).
 
 m <- matrix (v, nrow=50, byrow=TRUE)
 
-# ivars <- data.frame(xsize=50,ysize=50)
+## ivars <- data.frame(xsize=50,ysize=50)
 
 maxSugarDiscrete2d <-list(lattice=m)
 attr(maxSugarDiscrete2d, "type") <- "Discrete2d"
