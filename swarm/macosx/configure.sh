4f477fc63424b31b449eaf421f85e45217fb41af
northcob <northcob@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
2007-06-06 06:58:20 +0000

    update to MacOS 10.4 and Mac Intel
    git-svn-id: svn://svn.savannah.nongnu.org/swarm/trunk@7745 84d386b0-d280-4341-8980-c4cb7e1eb7a5

--- swarm/macosx/configure.sh
+++ swarm/macosx/configure.sh
@@ -1,22 +1,25 @@
 #!/bin/sh
 
-cd $BUILT_PRODUCTS_DIR/
+# cd $BUILT_PRODUCTS_DIR/  for older Xcode
+cd $BUILD_DIR/
 
-export MACOSX_DEPLOYMENT_TARGET=10.3
+export MACOSX_DEPLOYMENT_TARGET=10.4
 
 # Edit this configure command to reflect local 
 # library installation paths.
 # Optimise for G4 or G5 by adding stuff like -fast
 # -mcpuG5 -mtuneG5 etc to CFLAGS. (not tested!)
 
-../../configure JAVAC=jikes CC="gcc -B ../../tools" CFLAGS=" -g -O2 -Wno-long-double" \
---prefix=/usr/local/swarm2.2 \
---with-tcldir=/usr/local/tcl8.4.7 \
---with-tkdir=/usr/local/tcl8.4.7 \
---with-bltdir=/usr/local/tcl8.4.7 \
---with-pngdir=/usr/local/png_1.2.5 \
---with-hdf5dir=/usr/local/hdf5_1.6.1 \
---with-jdkdir=/System/Library/Frameworks/JavaVM.framework \
+../../configure JAVAC=/usr/bin/javac \
+ CC="gcc -B ../../tools" CFLAGS=" -g -Os -Wno-long-double -fnested-functions" \
+--prefix=/Library/Frameworks/Swarm.framework/Versions/2.2 \
+--with-tcldir=/Library/Frameworks/Tcl.framework/Versions/8.4 \
+--with-tkdir=/Library/Frameworks/Tk.framework/Versions/8.4-X11 \
+--with-bltdir=/Library/Frameworks/Tk.framework/Versions/8.4-X11 \
+--with-pngdir=/Library/Frameworks/Swarm.framework/Versions/Current/Resources \
+--with-ffidir=/Library/Frameworks/Swarm.framework/Versions/Current/Resources \
+--with-hdf5dir=/Library/Frameworks/Swarm.framework/Versions/Current/Resources \
+--without-jdkdir \
 --enable-onelib \
 --disable-fast-install \
 --disable-static 
@@ -25,9 +28,13 @@ export MACOSX_DEPLOYMENT_TARGET=10.3
 #--with-jdkdir=/System/Library/Frameworks/JavaVM.framework \
 #--with-ffidir=/usr/lib/gcc/powerpc-apple-darwin/4.0.0 \
 #--with-ffcalldir=/Users/billn/Public/Swarm/ffcall-1.9/avcall/build/dst \
+#--with-jdkdir=/System/Library/Frameworks/JavaVM.framework \
 #--disable-onelib \
 #--with-tcllibname=libtcl8.4g \
 #--with-tclscriptdir=/usr/local/tcl8.4.5/lib/tcl8.4 \
 #--with-tklibname=libtk8.4g \
 #--with-ffcalldir=/Groups/billn/Public/Swarm/ffcall-1.9/avcall/build/dst \
-#--without-hdf5dir \
+#--with-hdf5dir=/usr/local/hdf5_1.6.1 \
+
+
+
